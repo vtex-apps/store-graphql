23ad63df1f1762ee05a34fefed3a2e98df999e29
Maira Bello <maira.bello@vtex.com.br>
2017-01-18 14:03:49 -0300

    Render orders to test graphql api

--- render/Main.js
+++ render/Main.js
@@ -17,11 +17,23 @@ Product.propTypes = {
   description: React.PropTypes.string,
 }
 
-const Library = ({data: {products}}) => {
+const Library = ({data: {products, orders}}) => {
   return (
     <div>
       <h1>Products</h1>
       {products.map(book => <Product key={book.slug} {...book} />)}
+
+      <h1>Orders</h1>
+      <div>
+        {orders.map(order => {
+          return (
+            <div key={order.orderId}>
+              <h3>{order.orderId}</h3>
+              <div>{order.items.map(item => item.name).join(', ')}</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
@@ -35,6 +47,12 @@ const query = gql`{
     name,
     slug,
     description
+  },
+  orders {
+    orderId,
+    items {
+      name
+    }
   }
 }
 `
