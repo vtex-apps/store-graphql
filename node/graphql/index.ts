8082d6c64110a708148f674e35472558842702d6
Guilherme Rodrigues <gadr90@gmail.com>
2018-03-05 22:02:09 -0300

    Update to node 3 and graphql 1

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,7 +1,7 @@
-import './axiosConfig'
-import catalogQueries from './resolvers/catalog'
-import {mutations as checkoutMutations, queries as checkoutQueries} from './resolvers/checkout'
-import {mutations as profileMutations, queries as profileQueries} from './resolvers/profile'
+import '../axiosConfig'
+import catalogQueries from '../resolvers/catalog'
+import {mutations as checkoutMutations, queries as checkoutQueries} from '../resolvers/checkout'
+import {mutations as profileMutations, queries as profileQueries} from '../resolvers/profile'
 
 // tslint:disable-next-line:no-var-requires
 Promise = require('bluebird')
