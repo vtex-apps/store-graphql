32ee2152869c2054ef042237b4013397ec92b654
Breno Calazans <brenocalazans@gmail.com>
2018-03-21 09:21:16 -0300

    Add banner message for context

--- node/axiosConfig.ts
+++ node/axiosConfig.ts
@@ -9,7 +9,8 @@ axios.interceptors.response.use(
     }
 
     const {config: { method, url }, status, data} = error.response
-    const message = JSON.stringify({ method, status, url, data }, null, 2)
+    const banner = 'External HTTP request failed with error:\n'
+    const message = banner + JSON.stringify({ method, status, url, data }, null, 2)
     throw new ResolverError(message, status)
   },
 )
