947b50d7764135d6f3188cde156f3a735f1330b6
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-04-12 10:12:09 -0300

    Fix the return of createDocument mutation to fill the graphQL type

--- node/resolvers/masterdata/index.ts
+++ node/resolvers/masterdata/index.ts
@@ -1,33 +1,40 @@
 import http from 'axios'
 import paths from '../paths'
-import {map, keys, zipObj, mergeAll} from 'ramda'
+import {zipObj, mergeAll} from 'ramda'
 import { withAuthToken, headers } from '../headers'
+
+/**
+ * Map a document object to a list of {key: 'property', value: 'propertyValue'}.
+ */
+const mapKeyValues = (document) => Object.keys(document).map(key => ({
+  key,
+  value: document[key]
+}))
+
+/* 
+ * Convert a list of fields like [ {key: 'propertyName', value: 'String'}, ... ]
+ * to a JSON format. 
+ */
+const parseFieldsToJson = (fields) => mergeAll(
+  fields.map(field => zipObj([field.key], [field.value]))
+)
 
 export const queries = {
   documents: async (_, args, { vtex: ioContext, request: {headers: {cookie}}}) => {
     const {acronym, fields} = args
     const url = paths.searchDocument(ioContext.account, acronym, fields)
-    const {data} = await http.get(url, {headers: withAuthToken()(ioContext, cookie) })
+    const {data} = await http.get(url, {headers: withAuthToken()(ioContext, cookie)})
     return data.map(document => ({
       id: document.id,
-      fields: Object.keys(document).map(key => ({
-        key,
-        value: document[key]
-      }))
+      fields: mapKeyValues(document)
     }))
   },
 
   document: async (_, args, { vtex: ioContext, request: {headers: {cookie}}}) => {
     const {acronym, fields, id} = args
     const url = paths.document(ioContext.account, acronym, fields, id)
-    const {data} = await http.get(url, {headers: withAuthToken()(ioContext, cookie) })
-    return {
-      id: data.id,
-      fields: Object.keys(data).map(key => ({
-        key,
-        value: data[key]
-      }))
-    }
+    const {data} = await http.get(url, {headers: withAuthToken()(ioContext, cookie)})
+    return {id: data.id, fields: mapKeyValues(data)}
   },
 }
 
@@ -35,11 +42,10 @@ export const mutations = {
   createDocument: async (_, args, { vtex: ioContext, request: {headers: {cookie}}}) => {
     const {acronym, documents: {fields}} = args
     const url = paths.documents(ioContext.account, acronym)
-    const body = mergeAll(fields.map(field => zipObj([field.key], [field.value])))
-    const {data} = await http.post(url, body, {headers: withAuthToken()(ioContext, cookie) })
-    console.log(data)
-    // return {
-    //   id: data.
-    // }
+    const {data: {Id, Href, DocumentId}} = await http.post(
+      url, parseFieldsToJson(fields), 
+      {headers: withAuthToken()(ioContext, cookie)}
+    )
+    return {id: Id, href: Href, documentId: DocumentId}
   } 
 }
