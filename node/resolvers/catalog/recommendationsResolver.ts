fab94a744a8b9fec3a229363f36bcfdcc89c0a81
Estácio Pereira <estacio.pereira@gmail.com>
2018-06-12 17:22:25 -0300

    Prettier code format (node/resolvers/catalog)

--- node/resolvers/catalog/recommendationsResolver.ts
+++ node/resolvers/catalog/recommendationsResolver.ts
@@ -1,17 +1,26 @@
-import axios from 'axios'
-import {map, prop} from 'ramda'
-import paths from '../paths'
-import {resolveLocalProductFields} from './fieldsResolver'
-import {withAuthToken} from '../headers'
+import axios from 'axios';
+import { map } from 'ramda';
+
+import { withAuthToken } from '../headers';
+import paths from '../paths';
+import { resolveLocalProductFields } from './fieldsResolver';
 
 export const resolveView = async (ioContext, product) => {
-  const url = paths.crossSelling(ioContext.account, product.productId, 'whosawalsosaw')
-  const {data} = await axios.get(url, { headers: withAuthToken()(ioContext) })
+  const url = paths.crossSelling(
+    ioContext.account,
+    product.productId,
+    'whosawalsosaw'
+  )
+  const { data } = await axios.get(url, { headers: withAuthToken()(ioContext) })
   return map(resolveLocalProductFields, data)
 }
 
 export const resolveBuy = async (ioContext, product) => {
-  const url = paths.crossSelling(ioContext.account, product.productId, 'whoboughtalsobought')
-  const {data} = await axios.get(url, { headers: withAuthToken()(ioContext) })
+  const url = paths.crossSelling(
+    ioContext.account,
+    product.productId,
+    'whoboughtalsobought'
+  )
+  const { data } = await axios.get(url, { headers: withAuthToken()(ioContext) })
   return map(resolveLocalProductFields, data)
 }
