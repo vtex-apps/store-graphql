efa5ae6ab9bb8ea260012989bc23ab8db1a2113d
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-06-18 17:51:28 -0300

    Change profile requests to use  ioContext headers

--- node/resolvers/profile/index.ts
+++ node/resolvers/profile/index.ts
@@ -1,5 +1,4 @@
 import http from 'axios'
-import { parse as parseCookie } from 'cookie'
 import {head, values, pickBy, pipe, prop, find } from 'ramda'
 import { headers, withAuthAsVTEXID } from '../headers'
 import httpResolver from '../httpResolver'
@@ -8,7 +7,10 @@ import profileResolver from './profileResolver'
 import ResolverError from '../../errors/resolverError'
 
 const makeRequest = async (url, token, data?, method='GET') => http.request({
-  url, data, method, headers: { Authorization: token }
+  url, data, method, headers: { 
+    'Proxy-Authorization': token,
+    'VtexIdclientAutCookie': token
+  }
 })
 
 const getClientData = async (account, token) => {
@@ -18,16 +20,6 @@ const getClientData = async (account, token) => {
   return await makeRequest(
     paths.profile(account).filterUser(user), token
   ).then(pipe(prop('data'), head))
-}
-
-const getClientToken = (cookie, account) => {
-  const parsedCookies = parseCookie(cookie || '')
-  const startsWithVtexId = (val, key) => key.startsWith(`VtexIdclientAutCookie_${account}`)
-  const token = head(values(pickBy(startsWithVtexId, parsedCookies)))
-  if (!token) {
-    throw new ResolverError('User is not authenticated.', 401)
-  }
-  return token
 }
 
 const getUserAdress = async (account, userId, token) => await makeRequest(
@@ -41,11 +33,10 @@ const isUserAddress = async (account, clientId, addressId, token) => find(
 )
 
 const addressPatch = async (_, args, config) => {
-  const { vtex: { account }, request: { headers: { cookie } } } = config
-  const token = getClientToken(cookie, account)
-  const { userId, id } = await getClientData(account, token)
+  const { vtex: { account, authToken }, request: { headers: { cookie } } } = config
+  const { userId, id } = await getClientData(account, authToken)
 
-  if (args.id && !(await isUserAddress(account, id, args.id, token))) {  
+  if (args.id && !(await isUserAddress(account, id, args.id, authToken))) {  
     throw new ResolverError('Address not found.', 400)
   }
 
@@ -62,30 +53,26 @@ const addressPatch = async (_, args, config) => {
 export const mutations = {
   createAddress: async (_, args, config) => addressPatch(_, args, config),
 
-  deleteAddress: async(_, { id: addressId }, config) => {
-    const { vtex: { account }, request: { headers: { cookie } } } = config
-    const token = getClientToken(cookie, account)
-    const { userId, id: clientId } = await getClientData(account, token)
+  deleteAddress: async(_, { id: addressId }, { vtex: { account, authToken }, request: { headers: { cookie } } }) => {
+    const { userId, id: clientId } = await getClientData(account, authToken)
   
-    if (!(await isUserAddress(account, clientId, addressId, token)) {  
+    if (!(await isUserAddress(account, clientId, addressId, authToken))) {  
       throw new ResolverError('Address not found.', 400)
     }
 
     return await makeRequest(
-      paths.profile(account).address(addressId), token, null, 'DELETE'
+      paths.profile(account).address(addressId), authToken, null, 'DELETE'
     )
   },
 
   updateAddress: async (_, args, config) => addressPatch(_, args, config),
 
-  updateProfile: async (_, args, { vtex: {account}, request: { headers: { cookie } } }) => {
-    const clientToken = getClientToken(cookie, account)
-
-    const { id: profileId } = await getClientData(account, clientToken)
+  updateProfile: async (_, args, { vtex: { account, authToken }, request: { headers: { cookie } } }) => {
+    const { id: profileId } = await getClientData(account, authToken)
     
     return await makeRequest(
-      paths.profile(account).profile(profileId),clientToken, args.fields, 'PATCH'
-    ).then(() => getClientData(account, clientToken))
+      paths.profile(account).profile(profileId), authToken, args.fields, 'PATCH'
+    ).then(() => getClientData(account, authToken))
   },
 }
 
