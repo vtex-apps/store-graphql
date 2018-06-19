908c352f63154411f4513a0472547453207ed101
Andre Abrantes <contact@andreabrantes.com>
2018-06-19 14:04:51 -0300

    Fix profile resolver to access only logged user information
    * Fix profile mutations authorization
    
    * Add corporate fields to profile query
    
    * Fix profile mutations to remove direct id access
    
    * Remove email from profile input and fix delete address
    
    * Change profile requests to use  ioContext headers

--- node/resolvers/profile/index.ts
+++ node/resolvers/profile/index.ts
@@ -1,39 +1,79 @@
-import { merge } from 'ramda'
-import { headers, withAuthToken } from '../headers'
+import http from 'axios'
+import {head, values, pickBy, pipe, prop, find } from 'ramda'
+import { headers, withAuthAsVTEXID } from '../headers'
 import httpResolver from '../httpResolver'
 import paths from '../paths'
 import profileResolver from './profileResolver'
+import ResolverError from '../../errors/resolverError'
+
+const makeRequest = async (url, token, data?, method='GET') => http.request({
+  url, data, method, headers: { 
+    'Proxy-Authorization': token,
+    'VtexIdclientAutCookie': token
+  }
+})
+
+const getClientData = async (account, token) => {
+  const { data: { user } } = await makeRequest(
+    paths.identity(account, { token }), token
+  )
+  return await makeRequest(
+    paths.profile(account).filterUser(user), token
+  ).then(pipe(prop('data'), head))
+}
+
+const getUserAdress = async (account, userId, token) => await makeRequest(
+  paths.profile(account).filterAddress(userId), token
+).then(prop('data'))
+
+
+const isUserAddress = async (account, clientId, addressId, token) => find(
+  address => address.id === addressId,
+  await getUserAdress(account, clientId, token)
+)
+
+const addressPatch = async (_, args, config) => {
+  const { vtex: { account, authToken }, request: { headers: { cookie } } } = config
+  const { userId, id } = await getClientData(account, authToken)
+
+  if (args.id && !(await isUserAddress(account, id, args.id, authToken))) {  
+    throw new ResolverError('Address not found.', 400)
+  }
+
+  const { DocumentId } = await httpResolver({
+    data: { ...args.fields, userId },
+    headers: withAuthAsVTEXID(headers.profile),
+    method: 'PATCH',
+    url: account => paths.profile(account).address(args.id || ''),
+  })(_, args, config)
+  
+  return await profileResolver(_, args, config)
+}
 
 export const mutations = {
-  createAddress: httpResolver({
-    data: ({ fields }) => fields,
-    headers: withAuthToken(headers.profile),
-    merge: ({ id, fields }) => merge({ id }, fields),
-    method: 'PATCH',
-    url: account => paths.profile(account).address(''),
-  }),
+  createAddress: async (_, args, config) => addressPatch(_, args, config),
 
-  deleteAddress: httpResolver({
-    headers: withAuthToken(headers.profile),
-    method: 'DELETE',
-    url: (account, { id }) => paths.profile(account).address(id),
-  }),
+  deleteAddress: async(_, { id: addressId }, { vtex: { account, authToken }, request: { headers: { cookie } } }) => {
+    const { userId, id: clientId } = await getClientData(account, authToken)
+  
+    if (!(await isUserAddress(account, clientId, addressId, authToken))) {  
+      throw new ResolverError('Address not found.', 400)
+    }
 
-  updateAddress: httpResolver({
-    data: ({ fields }) => fields,
-    headers: withAuthToken(headers.profile),
-    merge: ({ id, fields }) => merge({ id }, fields),
-    method: 'PATCH',
-    url: (account, { id }) => paths.profile(account).address(id),
-  }),
+    return await makeRequest(
+      paths.profile(account).address(addressId), authToken, null, 'DELETE'
+    )
+  },
 
-  updateProfile: httpResolver({
-    data: ({ fields }) => fields,
-    headers: withAuthToken(headers.profile),
-    merge: ({ id, fields }) => merge({ id }, fields),
-    method: 'PATCH',
-    url: (account, { id }) => paths.profile(account).profile(id),
-  }),
+  updateAddress: async (_, args, config) => addressPatch(_, args, config),
+
+  updateProfile: async (_, args, { vtex: { account, authToken }, request: { headers: { cookie } } }) => {
+    const { id: profileId } = await getClientData(account, authToken)
+    
+    return await makeRequest(
+      paths.profile(account).profile(profileId), authToken, args.fields, 'PATCH'
+    ).then(() => getClientData(account, authToken))
+  },
 }
 
 export const queries = {
