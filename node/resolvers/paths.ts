95d6ebe227070985b9bb3d8f4e1f235c1538533a
Bruno Dias <bruno.dias@vtex.com.br>
2018-06-22 17:09:22 -0300

    Refact auth resolver

--- node/resolvers/paths.ts
+++ node/resolvers/paths.ts
@@ -64,8 +64,8 @@ const paths = {
   identity: (account, { token }) => `${paths.vtexId()}/authenticated/user?authToken=${encodeURIComponent(token)}`,
   getTemporaryToken: (scope, account) => `${paths.vtexId()}/authentication/start?appStart=true&scope=${scope}&accountName=${account}`,
   sendEmailVerification: (email, token) => `${paths.vtexId()}/authentication/accesskey/send?authenticationToken=${token}&email=${email}`,
-  accessKeySignIn: (email, token, code) => `${paths.vtexId()}/authentication/accesskey/validate?authenticationToken=${token}&login=${email}&accesskey=${code}`,
-  classicSignIn: (email, token, password) => `${paths.vtexId()}/authentication/classic/validate?authenticationToken=${token}&login=${email}&password=${password}`,
+  accessKeySignIn: (token, email, code) => `${paths.vtexId()}/authentication/accesskey/validate?authenticationToken=${token}&login=${email}&accesskey=${code}`,
+  classicSignIn: (token, email, password) => `${paths.vtexId()}/authentication/classic/validate?authenticationToken=${token}&login=${email}&password=${password}`,
 
   /** Master Data API v1
    * Docs: https://documenter.getpostman.com/view/164907/masterdata-api-v102/2TqWsD
