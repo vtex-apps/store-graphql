5eddef8c7e9325951c8112ebf4ab681d62949dd8
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-06-28 17:15:55 -0300

    Integrate benefits with product in catalog resolver

--- node/resolvers/benefits/index.ts
+++ node/resolvers/benefits/index.ts
@@ -2,11 +2,38 @@ import httpResolver from "../httpResolver"
 import { headers, withAuthToken } from "../headers"
 import paths from '../paths'
 
+/**
+ * Default values for Shipping Items
+ */
+const DEFAULT_QUANTITY = '1'
+const DEFAULT_SELLER = '1'
+
 export const queries = {
+  /**
+   * Benefits Resolver
+   * There are two ways to make the request data
+   * First of them is passing the graphql args with the items property
+   * which is an array of Shipping Items.
+   * Second of them is passing just the id of the product itself as a 
+   * graphql args.
+   */
   benefits: httpResolver({
-    data: ({ items }) => ({
-      items
-    }),
+    data: (args) => {
+      if (args.items) {
+        return {
+          items: args.items
+        }
+      }
+      return {
+        items: [
+          { 
+            id: args.id,
+            quantity: DEFAULT_QUANTITY,
+            seller: DEFAULT_SELLER
+          }
+        ]
+      }
+    },
     merge: (args, data) => data.ratesAndBenefitsData.teaser,
     headers: withAuthToken(headers.json),
     url: paths.benefits,
