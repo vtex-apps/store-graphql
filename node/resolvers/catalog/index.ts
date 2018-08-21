4f60296ce839a999920b7c34b0ff786471f8f2e1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-08-21 10:16:52 -0300

    Change facets return to filtered facets with rest

--- node/resolvers/catalog/index.ts
+++ node/resolvers/catalog/index.ts
@@ -5,8 +5,8 @@ import { compose, equals, find, head, map, prop, split, test } from 'ramda'
 import * as slugify from 'slugify'
 
 import ResolverError from '../../errors/resolverError'
-import { withAuthToken } from '../headers'
 import { queries as benefitsQueries } from '../benefits'
+import { withAuthToken } from '../headers'
 import paths from '../paths'
 import { resolveBrandFields, resolveCategoryFields, resolveFacetFields, resolveProductFields } from './fieldsResolver'
 
@@ -196,24 +196,33 @@ export const queries = {
     return map(resolveCategoryFields, categories)
   },
 
-  search: async (_, data, { vtex: ioContext }: ColossusContext, info) => {
-    const { map, query, rest } = data
-    const facetsMap = map.split(',').slice(0, query.split('/').length).join(',')
+  search: async (_, data, colossusContext: ColossusContext, info) => {
+    const { map: mapParams, query, rest } = data
+    const facetsMap = mapParams.split(',').slice(0, query.split('/').length).join(',')
     const queryWithRest = query + (rest && '/' + rest.replace(/,/g, '/'))
     const facetsValue = query + '?map=' + facetsMap
-    const facetsValueWithRest = queryWithRest + '?map=' + map
-    const productsPromise = queries.products(_, { ...data, query: queryWithRest }, { vtex: ioContext }, info)
-    const facetsPromise = queries.facets(_, { facets: facetsValue }, { vtex: ioContext })
+    const facetsValueWithRest = queryWithRest + '?map=' + mapParams
+
+    const productsPromise = queries.products(_, { ...data, query: queryWithRest }, colossusContext, info)
+    const facetsPromise = queries.facets(_, { facets: facetsValue }, colossusContext)
     const categoriesPromise = queries.categories(_, {
       treeLevel: query.split('/').length
-    }, { vtex: ioContext })
-    const facetsWithRestPromise = queries.facets(_, { facets: facetsValueWithRest }, { vtex: ioContext })
+    }, colossusContext)
+    const facetsWithRestPromise = queries.facets(_, { facets: facetsValueWithRest }, colossusContext)
+
     const [products, facets, facetsWithRest, categories] = await Promise.all([
       productsPromise, facetsPromise, facetsWithRestPromise, categoriesPromise
     ])
     const { titleTag, metaTagDescription } = findInTree(categories, query.split('/'))
     const recordsFiltered = facetsWithRest.Departments.reduce((total, dept) => total + dept.Quantity, 0)
-    return { facets, products, recordsFiltered, titleTag, metaTagDescription }
+
+    return {
+      facets: facetsWithRest,
+      metaTagDescription,
+      products,
+      recordsFiltered,
+      titleTag,
+    }
   },
 
   searchContextFromParams: async (_, args, { vtex: ioContext }: ColossusContext) => {
