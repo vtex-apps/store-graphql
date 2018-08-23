9fa6bcd94a9e34709101676f486910c261835b94
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-23 16:23:52 -0300

    Remove await

--- node/resolvers/logistics/index.ts
+++ node/resolvers/logistics/index.ts
@@ -3,11 +3,10 @@ import { withAuthToken } from '../headers'
 import httpResolver from '../httpResolver'
 
 export const queries = {
-  logistics: async (_, args, config) => {
-    return await httpResolver({
+  logistics: (_, args, config) =>
+    httpResolver({
       headers: withAuthToken()(config.vtex),
       method: 'GET',
       url: account => paths.logistics(account).shipping,
-    })(_, args, config)
-  },
+    })(_, args, config),
 }
