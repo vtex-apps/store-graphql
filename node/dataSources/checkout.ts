5e2fd0383f93dfb2693cdeb0a7f380fbbe0bec8b
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-09-14 17:39:11 -0300

    Create CheckoutDataSource

---
+++ node/dataSources/checkout.ts
@@ -0,0 +1,133 @@
+import { Request, RequestOptions, Response, RESTDataSource } from 'apollo-datasource-rest'
+import { ColossusContext } from 'colossus'
+import { forEachObjIndexed } from 'ramda'
+
+export interface SimulationData {
+  country: string
+  items: any[]
+  postalCode: string
+}
+
+const SetCookieWhitelist = [
+  'checkout.vtex.com',
+  '.ASPXAUTH',
+]
+
+const isWhitelistedSetCookie = (cookie: string) => {
+  const [key] = cookie.split('=')
+  return SetCookieWhitelist.includes(key)
+}
+
+export class CheckoutDataSource extends RESTDataSource<ColossusContext> {
+  constructor() {
+    super()
+  }
+
+  public addItem = (orderFormId: string, items: any) => this.post(
+    `/pub/orderForm/${orderFormId}/items`,
+    {
+      expectedOrderFormSections: ['items'],
+      orderItems: items,
+    }
+  )
+
+  public cancelOrder = (orderFormId: string, reason: string) => this.post(
+    `/pub/orders/${orderFormId}/user-cancel-request`,
+    {reason},
+  )
+
+  public setOrderFormCustomData = (orderFormId: string, appId: string, field: string, value: any) => this.put(
+    `/pub/orderForm/${orderFormId}/customData/${appId}/${field}`,
+    {
+      expectedOrderFormSections: ['customData'],
+      value,
+    }
+  )
+
+  public updateItems = (orderFormId: string, orderItems: any) => this.post(
+    `/pub/orderForm/${orderFormId}/items/update`,
+    {
+      expectedOrderFormSections: ['items'],
+      orderItems,
+    }
+  )
+
+  public updateOrderFormIgnoreProfile = (orderFormId: string, ignoreProfileData: boolean) => this.patch(
+    `/pub/orderForm/${orderFormId}/profile`,
+    {
+      expectedOrderFormSections: ['items'],
+      ignoreProfileData,
+    }
+  )
+
+  public updateOrderFormPayment = (orderFormId: string, payments: any) => this.post(
+    `/pub/orderForm/${orderFormId}/attachments/paymentData`,
+    {
+      expectedOrderFormSections: ['items'],
+      payments,
+    }
+  )
+
+  public updateOrderFormProfile = (orderFormId: string, fields: any) => this.post(
+    `/pub/orderForm/${orderFormId}/attachments/clientProfileData`,
+    {
+      expectedOrderFormSections: ['items'],
+      ...fields,
+    }
+  )
+
+  public updateOrderFormShipping = (orderFormId: string, address: any) => this.post(
+    `/pub/orderForm/${orderFormId}/attachments/shippingData`,
+    {
+      expectedOrderFormSections: ['items'],
+      ...address,
+    }
+  )
+
+  public orderForm = () => this.post(
+    `/pub/orderForm`,
+    {expectedOrderFormSections: ['items']},
+  )
+
+  public orders = () => this.get(
+    '/pub/orders'
+  )
+
+  public shipping = (simulation: SimulationData) => this.post(
+    '/pub/orderForms/simulation',
+    simulation,
+  )
+
+  get baseURL() {
+    const {vtex: {account}} = this.context
+    return `http://${account}.vtexcommercestable.com.br/api/checkout`
+  }
+
+  protected async didReceiveResponse (response: Response, request: Request) {
+    const result = await super.didReceiveResponse(response, request)
+
+    const rawHeaders = (response.headers as any).raw() as Record<string, any>
+    const responseSetCookies: string[] = rawHeaders ? rawHeaders['set-cookie'] : []
+    const forwardedSetCookies = responseSetCookies.filter(isWhitelistedSetCookie)
+    if (forwardedSetCookies.length > 0) {
+      this.context.set('set-cookie', forwardedSetCookies)
+    }
+
+    return result
+  }
+
+  protected willSendRequest (request: RequestOptions) {
+    const {vtex: {authToken}, headers} = this.context
+    forEachObjIndexed(
+      (value: string, header) => request.headers.set(header, value),
+      {
+        Accept: 'application/json',
+        Authorization: authToken,
+        'Content-Type': 'application/json',
+        'Cookie': headers.cookie,
+        Host: headers['x-forwarded-host'],
+        'Proxy-Authorization': authToken,
+      }
+    )
+  }
+}
