b925432e02a226029b00f927320db7f1ad7d1e51
amoreira <anderson.moreira@vtex.com.br>
2018-09-26 18:33:57 -0300

    Filter installments by criteria

---
+++ node/resolvers/catalog/offer.ts
@@ -0,0 +1,25 @@
+import {comparator, filter, gte, head, lte, sort} from 'ramda'
+
+const InstallmentsCriteria = {
+  ALL: 'ALL',
+  MAX: 'MAX',
+  MIN: 'MIN',
+}
+
+export const resolvers = {
+  Offer: {
+    Installments: ({Installments}, args, __) => {
+      const {criteria, rates} = args
+      if (criteria === InstallmentsCriteria.ALL) {
+        return Installments
+      }
+      const filteredInstallments = !rates
+        ? Installments
+        : filter(({InterestRate}) => !InterestRate, Installments)
+
+      const compareFunc = criteria === InstallmentsCriteria.MAX ? gte : lte
+      const byNumberOfInstallments = comparator((previous, next) => compareFunc(previous.NumberOfInstallments, next.NumberOfInstallments))
+      return [head(sort(byNumberOfInstallments, filteredInstallments))]
+    },
+  }
+}
