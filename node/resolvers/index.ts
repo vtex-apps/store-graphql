bf3afab70ac84482e729afb49cd77866ac93d7c5
vitoriaheliane <vitoriaheliane@gmail.com>
2018-10-15 11:41:31 -0300

    Add createwishList resolve

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -6,8 +6,12 @@ import { mutations as documentMutations, queries as documentQueries } from './do
 import { queries as logisticsQueries } from './logistics'
 import { mutations as profileMutations, queries as profileQueries, rootResolvers as profileRootResolvers } from './profile'
 import { mutations as sessionMutations, queries as sessionQueries } from './session'
+<<<<<<< HEAD
 import { fieldResolvers as subscriptionsFieldResolvers, mutations as subscriptionsMutations, queries as subscriptionsQueries } from './subscriptions'
 import { queries as wishListQueries } from './wishList'
+=======
+import { queries as wishListQueries, mutation as wishListMutations } from './wishList'
+>>>>>>> Add createwishList resolve
 
 // tslint:disable-next-line:no-var-requires
 Promise = require('bluebird')
@@ -24,7 +28,11 @@ export const resolvers = {
     ...authMutations,
     ...documentMutations,
     ...sessionMutations,
+<<<<<<< HEAD
     ...subscriptionsMutations
+=======
+    ...wishListMutations
+>>>>>>> Add createwishList resolve
   },
   Query: {
     ...catalogQueries,
