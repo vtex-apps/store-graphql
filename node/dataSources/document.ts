beeba3dba551be11eb8b34f0a3e0012c931a1bcb
Vitoria Heliane <vitoriaheliane@gmail.com>
2018-11-29 09:23:09 -0300

    Update document's queries to use the document dataSource

--- node/dataSources/document.ts
+++ node/dataSources/document.ts
@@ -5,8 +5,25 @@ export class DocumentDataSource extends RESTDataSource<ServiceContext> {
         super()
     }
 
-    get baeURL() {
+    public getDocument = (acronym, id, fields) => this.get(
+        `${acronym}/documents/${id}?_fields=${fields}`
+    )
+
+    public getDocuments = acronym => this.get(
+        `${acronym}/documents`
+    )
+    
+    public searchDocuments = (acronym, fields, where) => this.get(
+        `${acronym}/search?_fields=${fields}${where ? `&_where=${encodeURIComponent(where)}` : ''}`
+    )
+
+    get baseURL() {
         const { vtex: { account } } = this.context
         return `http://api.vtex.com/${account}/dataentities/`
+    }
+
+    willSendRequest(request) {
+        request.headers.set('Authorization', this.context.vtex.authToken)
+        request.headers.set('Accept', 'application/vnd.vtex.ds.v10+json')
     }
 }
