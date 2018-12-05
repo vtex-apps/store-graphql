655c91e17831bb14be4011fdd212cabf1b00baab
Joao Fidelis <jgfidelis@gmail.com>
2018-12-05 14:59:50 -0200

    add types to pickup points query

--- node/resolvers/logistics/index.ts
+++ node/resolvers/logistics/index.ts
@@ -1,24 +1,94 @@
-import { headers, withAuthAsVTEXID, withAuthToken,  } from '../headers'
+import { headers, withAuthToken,  } from '../headers'
 import httpResolver from '../httpResolver'
 import paths from '../paths'
 
-const parseLogisticAddress = (logAddress, idx) => ({
-  ...logAddress,
-  addressId: idx.toString(),
-  addressType: 'pickup',
-  country: logAddress.country.acronym,
-  geoCoordinates: [-43.1825731, -22.9460815],
-})
+interface CommonAddress {
+  postalCode: string,
+  city: string,
+  state: string,
+  neighborhood: string,
+  street: string,
+  number: string,
+  complement: string,
+  reference: string,
+}
 
-const parseItem = (logisticItem, idx) => ({
-  ...logisticItem,
-  address: parseLogisticAddress(logisticItem.address, idx),
-  friendlyName: logisticItem.name,
-})
+interface LogisticAddress extends CommonAddress {
+  country: { acronym: string, name: string },
+  location: { latitude: number, longitude: number },
+} 
+
+interface ParsedAddress extends CommonAddress {
+  geoCoordinates: number[],
+  addressId: string,
+  addressType: string,
+  country: string,
+}
+
+interface CommonPickupPoint {
+  id: string,
+  description: string | null,
+  instructions: string,
+  formatted_address: string | null,
+  isActive: boolean,
+  distance: number,
+  seller: string,
+  businessHours: Array<{ closingTime: string, openingTime: string, dayOfWeek: number }>,
+  tagsLabel: any[],
+  pickupHolidays: any[],
+}
+
+interface LogisticPickupPoint extends CommonPickupPoint {
+  name: string,
+  address: LogisticAddress,
+}
+
+interface ParsedPickupPoint extends CommonPickupPoint {
+  friendlyName: string,
+  address: ParsedAddress,
+}
+
+interface Paging {
+  page: number,
+  perPage: number,
+  total: number,
+  pages: number
+}
+
+interface LogisticOuput {
+  items: LogisticPickupPoint[],
+  paging: Paging,
+}
+
+const parseLogisticAddress = (logAddress: LogisticAddress, idx: number): ParsedAddress => {
+  const { country, location, ...rest } = logAddress
+  return {
+    ...rest,
+    addressId: idx.toString(),
+    addressType: 'pickup',
+    country: country.acronym,
+    geoCoordinates: [location.longitude, location.latitude],
+  }
+}
+
+const parseItem = (logisticItem: LogisticPickupPoint, idx: number): ParsedPickupPoint => {
+  const { address, name, ...rest } = logisticItem
+  return {
+    ...rest,
+    address: parseLogisticAddress(address, idx),
+    friendlyName: name,
+  }
+}
 
 interface PickupPointsArgs {
   lat: string,
   long: string,
+  maxDistance: number | undefined,
+}
+
+interface ParsedItems {
+  items: ParsedPickupPoint[],
+  paging: Paging,
 }
 
 export const queries = {
@@ -27,17 +97,15 @@ export const queries = {
     method: 'GET',
     url: (account: string) => paths.logisticsConfig(account).shipping,
   }),
-  pickupPoints: async (root, args: PickupPointsArgs, context) => {
+  pickupPoints: async (root, args: PickupPointsArgs, context): Promise<ParsedItems> => {
     const { vtex: ioContext, request } = context
     const fullHeader = withAuthToken(headers.json)(ioContext, request.headers.cookie)
-    const response = await httpResolver({
+    const response = await httpResolver<LogisticOuput>({
       headers: fullHeader,
       method: 'GET',
-      url: (account: string, { lat, long }: PickupPointsArgs) => paths.logisticsConfig(account).pickupPoints(lat, long),
+      url: (account: string, { lat, long, maxDistance = 10 }: PickupPointsArgs) => paths.logisticsConfig(account).pickupPoints(lat, long, maxDistance),
     })(root, args, context)
-    return { 
-      ...response, 
-      items: (response.items || []).map((item, idx) => parseItem(item, idx)),
-    }
-},
+    const parsedResponse = { ...response, items: (response.items || []).map((item, idx) => parseItem(item, idx)) }
+    return parsedResponse
+  },
 }
