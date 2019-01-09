c9f3fed430938891db660d7e3c6575b67c6070a7
Tiago Gimenes <tlgimenes@gmail.com>
2019-01-09 08:26:15 -0200

    removing try..catch statement in catalog proxy, since it's already held in prepare function

--- node/routes/catalogProxy.ts
+++ node/routes/catalogProxy.ts
@@ -8,40 +8,31 @@ const STALE_IF_ERROR_S = 20 * 60
 
 const isPlatformGC = account => account.indexOf('gc_') === 0 || account.indexOf('gc-') === 0
 
-export const catalogProxy = async (ctx: ServiceContext) => {
-  try {
-    const {vtex: {account, authToken, production, route: {params: {path}}}, headers: {cookie}, query} = ctx
+export const catalogProxy = async (ctx: Context) => {
+  const {vtex: {account, authToken, production, route: {params: {path}}}, headers: {cookie}, query} = ctx
 
-    const [host, basePath] = isPlatformGC(account)
-      ? ['api.gocommerce.com', `${account}/search`]
-      : [`${account}.vtexcommercestable.com.br`, 'api/catalog_system']
+  const [host, basePath] = isPlatformGC(account)
+    ? ['api.gocommerce.com', `${account}/search`]
+    : [`${account}.vtexcommercestable.com.br`, 'api/catalog_system']
 
-    const {data, headers} = await axios.request({
-      baseURL: `http://${host}/${basePath}`,
-      headers: {
-        'Authorization': authToken,
-        'Proxy-Authorization': authToken,
-        'X-VTEX-Proxy-To': `https://${host}`,
-        ...cookie && {cookie},
-      },
-      params: query,
-      paramsSerializer: (params) => qs.stringify(params, {arrayFormat: 'repeat'}),
-      timeout: TIMEOUT_MS,
-      url: encodeURI(path.trim()),
-    })
+  const {data, headers} = await axios.request({
+    baseURL: `http://${host}/${basePath}`,
+    headers: {
+      'Authorization': authToken,
+      'Proxy-Authorization': authToken,
+      'X-VTEX-Proxy-To': `https://${host}`,
+      ...cookie && {cookie},
+    },
+    params: query,
+    paramsSerializer: (params) => qs.stringify(params, {arrayFormat: 'repeat'}),
+    timeout: TIMEOUT_MS,
+    url: encodeURI(path.trim()),
+  })
 
-    keys(headers).forEach(headerKey => {
-      ctx.set(headerKey, headers[headerKey])
-    })
+  keys(headers).forEach(headerKey => {
+    ctx.set(headerKey, headers[headerKey])
+  })
 
-    ctx.set('cache-control', production ? `public, max-age=${MAX_AGE_S}, stale-if-error=${STALE_IF_ERROR_S}` : 'no-store, no-cache')
-    ctx.body = data
-  }
-  catch (err) {
-    console.log('Error in proxy catalog', err)
-    const {data, status} = err.response
-    ctx.body = data
-    ctx.status = status
-    ctx.set('Cache-Control', 'no-cache, no-store')
-  }
+  ctx.set('cache-control', production ? `public, max-age=${MAX_AGE_S}, stale-if-error=${STALE_IF_ERROR_S}` : 'no-store, no-cache')
+  ctx.body = data
 }
