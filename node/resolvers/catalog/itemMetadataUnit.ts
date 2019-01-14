a37eb1f4d0d7f513dc14f8082b83d23363cbf9d1
Joao Fidelis <joaogfidelis@gmail.com>
2019-01-14 12:03:22 -0200

    use MainImage from catalog

--- node/resolvers/catalog/itemMetadataUnit.ts
+++ node/resolvers/catalog/itemMetadataUnit.ts
@@ -1,13 +1,13 @@
 
 /** 
- * This is used because the itemMetadata that comes from the catalog has capitalized Name and ImageUrl.
- * The itemMetadata that comes from the checkout API has name and imageUrl. This unify the patters
+ * This is used because the itemMetadata that comes from the catalog has capitalized Name and MainImage.
+ * The itemMetadata that comes from the checkout API has name and imageUrl. This unify the patterns
  */
 
 export const resolvers = {
   ItemMetadataUnit: {
-    imageUrl: ({ imageUrl, ImageUrl }) => {
-      return imageUrl || ImageUrl
+    imageUrl: ({ imageUrl, MainImage }) => {
+      return imageUrl || MainImage
     },
     name: ({ name, Name }) => {
       return name || Name
