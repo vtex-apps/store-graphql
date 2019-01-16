d50bbb6e94025e5743cd0408b0ba7ae15fdb4be1
Felipe Sales <sales.felipeb@gmail.com>
2019-01-16 09:41:27 -0200

    Changed from withCurrentUser to withCurrentProfile

--- node/directives/withCurrentProfile.ts
+++ node/directives/withCurrentProfile.ts
@@ -3,24 +3,27 @@ import { SchemaDirectiveVisitor } from 'graphql-tools'
 
 import ResolverError from '../errors/resolverError'
 
-const getSessionCurrentUser = async (context) => {
+const getSessionCurrentProfile = async (context) : Promise<CurrentProfile>  => {
   const { dataSources: { session } } = context
 
   const currentSession = await session.getSession()
   if(!currentSession.namespaces || !currentSession.namespaces.cookie['VtexIdclientAutCookie'])
     throw new ResolverError(`ERROR no session`, currentSession)
 
-  const { namespaces: { impersonate: { storeUserEmail }, authentication: { adminUserEmail } } } = currentSession
+  const { namespaces: { profile: { email, id } } } = currentSession
 
-  return storeUserEmail && storeUserEmail.value || adminUserEmail.value
+  return {
+    email: email.value,
+    id: id.value
+  }
 }
 
-export class WithCurrentUser extends SchemaDirectiveVisitor {
+export class WithCurrentProfile extends SchemaDirectiveVisitor {
   public visitFieldDefinition (field: GraphQLField<any, any>) {
     const {resolve = defaultFieldResolver} = field
     field.resolve = async (root, args, ctx, info) => {
-      const currentUser = await getSessionCurrentUser(ctx) 
-      ctx.currentUser = currentUser
+      const currentProfile = await getSessionCurrentProfile(ctx) 
+      ctx.currentProfile = currentProfile
       
       return resolve(root, args, ctx, info)
     }
