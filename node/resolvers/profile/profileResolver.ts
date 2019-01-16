f7b3010ade8d0d2e175aec3391968a3c01421864
Felipe Sales <sales.felipeb@gmail.com>
2019-01-16 14:08:53 -0200

    Create fieldResolver for payments and add fix to the addresses

--- node/resolvers/profile/profileResolver.ts
+++ node/resolvers/profile/profileResolver.ts
@@ -1,5 +1,6 @@
 import http from 'axios'
-import { compose, head, join, mapObjIndexed, merge, pick, pipe, pluck, prop, split, values } from 'ramda'
+import { compose, head, join, mapObjIndexed, pick, pipe, pluck, prop, split, values } from 'ramda'
+
 import paths from '../paths'
 
 export const pickCustomFieldsFromData = (customFields: string, data) => customFields && compose(
@@ -13,50 +14,68 @@ export const customFieldsFromGraphQLInput = (customFieldsInput) => compose(
   pluck('key')
 )(customFieldsInput)
 
-export const getProfileData = async (ctx, { customFields }, userEmail) => {
-  const config = {
+const getProfileHeadersConfig = (context) => {
+  return {
     headers: {
-      'Proxy-Authorization': ctx.authToken,
-      vtexidclientautcookie: ctx.authToken,
+      'Proxy-Authorization': context.authToken,
+      vtexidclientautcookie: context.authToken,
     },
   }
+}
 
-  const profileURL = paths.profile(ctx.account).filterUser(userEmail, customFields)
+// This fix is necessary because some addresses were saved with the profile.id as userId instead of the actual profile.userId
+// TO DO: Remove this on the Future
+const fixAddresses = async (context, currentProfile: CurrentProfile, profileId: String) => {
+  const config = getProfileHeadersConfig(context)
+  const addressURL = paths.profile(context.account).filterAddress(profileId)
+
+  const addressesToFix = await http.get(addressURL, config).then(prop('data'))
+
+  addressesToFix.map(async (address) => {
+    await http.patch(paths.profile(context.account).address(address.id),{ userId: currentProfile.userId }, config)
+  })
+}
+
+export const getProfileData = async (context, { customFields }, currentProfile: CurrentProfile) => {
+  const config = getProfileHeadersConfig(context)
+
+  const profileURL = paths.profile(context.account).filterUser(currentProfile.email, customFields)
   const profileData = await http.get(profileURL, config).then<any>(pipe(prop('data'), head))
 
-  if (profileData && profileData.id) {
-    profileData.customFields = pickCustomFieldsFromData(customFields, profileData)
-
-    profileData.profilePicture = profileData.profilePicture
-      && `//api.vtex.com/${ctx.account}/dataentities/CL/documents/${profileData.id}/profilePicture/attachments/${profileData.profilePicture}`
-
-    const addressURL = paths.profile(ctx.account).filterAddress(profileData.id)
-    const address = profileData && await http.get(addressURL, config).then(prop('data'))
-
-    const paymentsURL = paths.profile(ctx.account).payments(profileData.userId)
-    const { data: paymentsRawData } = await http.get(paymentsURL, config)
-    let payments = null
-
-    if(paymentsRawData) {
-      const availableAccounts = JSON.parse(paymentsRawData.paymentData).availableAccounts
-      payments = availableAccounts.map((account) => {
-        const {bin, availableAddresses, accountId, ...cleanAccount} = account
-        const accountAddress = address.find(
-          (addr) => addr.addressName === availableAddresses[0]
-        )
-        return {...cleanAccount, id: accountId, address: accountAddress}
-      })
-    }
-
-    const result = merge({ payments, address, cacheId: userEmail }, profileData)
-
-    console.log('result >>>>> ', result)
-
-    return result
+  if(profileData && profileData.userId) {
+    profileData.address = await getAddresses(context, currentProfile, profileData.id)
   }
 
-  return {
-    cacheId: userEmail,
-    email: userEmail
+  return profileData
+}
+
+export const getAddresses = async (context, currentProfile: CurrentProfile, profileId) => {
+  const config = getProfileHeadersConfig(context)
+
+  await fixAddresses(context,currentProfile, profileId)
+
+  const addressURL = paths.profile(context.account).filterAddress(currentProfile.userId)
+
+  return await http.get(addressURL, config).then(prop('data'))
+}
+
+export const getPayments = async (context, userId: String, addresses) => {
+  const config = getProfileHeadersConfig(context)
+
+  const paymentsURL = paths.profile(context.account).payments(userId)
+  const { data: paymentsRawData } = await http.get(paymentsURL, config)
+
+  let payments = null
+  if(paymentsRawData) {
+    const availableAccounts = JSON.parse(paymentsRawData.paymentData).availableAccounts
+    payments = availableAccounts.map((account) => {
+      const {bin, availableAddresses, accountId, ...cleanAccount} = account
+      const accountAddress = addresses.find(
+        (addr) => addr.addressName === availableAddresses[0]
+      )
+      return {...cleanAccount, id: accountId, address: accountAddress}
+    })
   }
+
+  return payments
 }
