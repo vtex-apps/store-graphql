940b606adf510fc29070bc4bd1b1584220e67e4b
Felipe Sales <sales.felipeb@gmail.com>
2019-01-22 14:25:55 -0200

    Validate by obj.id

--- node/resolvers/profile/fieldResolvers.ts
+++ node/resolvers/profile/fieldResolvers.ts
@@ -5,10 +5,10 @@ import { getPayments, getAddresses, pickCustomFieldsFromData } from './services'
 export default {
   Profile: {
     cacheId: prop('email'),
-    profilePicture: (obj, args, context, info) =>  obj.profilePicture && obj.id
+    profilePicture: (obj, args, context) =>  obj.profilePicture && obj.id
        && `//api.vtex.com/${context.vtex.account}/dataentities/CL/documents/${obj.id}/profilePicture/attachments/${obj.profilePicture}`,
-    payments: (obj, args, context, info) => obj.userId && getPayments(context, obj.id),
-    address: (obj, args, context, info) => obj.userId && getAddresses(context, obj.id),
+    payments: (obj, args, context) => obj.id && getPayments(context, obj.id),
+    address: (obj, args, context) => obj.id && getAddresses(context, obj.id),
     customFields: (obj) =>  { 
       if (typeof obj.customFields === 'string')
         return pickCustomFieldsFromData(obj.customFields, obj)
