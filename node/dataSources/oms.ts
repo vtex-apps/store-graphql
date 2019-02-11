777e8ccfcb05455f842062e8995a11616131d8d9
Joao Fidelis <joaogfidelis@gmail.com>
2019-02-11 17:19:26 -0200

    implement suggestions, rename to userLastOrder

--- node/dataSources/oms.ts
+++ node/dataSources/oms.ts
@@ -1,27 +1,28 @@
 import { RequestOptions, RESTDataSource } from 'apollo-datasource-rest'
-import * as cookies from 'cookie'
 import { forEachObjIndexed } from 'ramda'
 
+const DEFAULT_TIMEOUT_MS = 4 * 1000
 
 export class OMSDataSource extends RESTDataSource<Context> {
-  public lastUserOrder = () => this.get(`/user/orders/last`)
+  public userLastOrder = () => this.get('user/orders/last')
 
   get baseURL() {
     const { vtex: { account } } = this.context
-
     return `http://${account}.vtexcommercestable.com.br/api/oms`
   }
 
   protected willSendRequest(request: RequestOptions) {
-    const { vtex: { authToken, account }, headers:{cookie} } = this.context
+    const { vtex: { account, authToken }, request: { headers: { cookie } } } = this.context
 
-    const parsedCookie = cookies.parse(cookie)
+    if (!request.timeout) {
+      request.timeout = DEFAULT_TIMEOUT_MS
+    }
 
     forEachObjIndexed(
       (value: string, header) => request.headers.set(header, value),
       {
+        'Cookie': cookie,
         'Proxy-Authorization': authToken,
-        'VtexIdClientAutCookie': parsedCookie.VtexIdclientAutCookie,
         'X-Vtex-Proxy-To': `http://${account}.vtexcommercestable.com.br`,
       }
     )
