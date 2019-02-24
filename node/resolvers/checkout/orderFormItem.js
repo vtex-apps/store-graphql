dce7b8509cd49157e725b97b51e24a46c5770310
Joao Fidelis <joaogfidelis@gmail.com>
2019-02-24 18:31:38 -0300

    add types

--- node/resolvers/checkout/orderFormItem.js
+++
@@ -1,8 +0,0 @@
-export const resolvers = {
-  OrderFormItem: {
-    assemblyOptions: ({ assemblyOptionsData: { childs, index, orderForm, hasAttachments }, ...item }) => ({ item, childs, index, orderForm, hasAttachments }),
-    listPrice: ({ listPrice }) => listPrice / 100,
-    price: ({ price }) => price / 100,
-    sellingPrice: ({ sellingPrice }) => sellingPrice / 100,
-  }
-}
