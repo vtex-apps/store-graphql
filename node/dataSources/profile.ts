06a2e45c8669dde99d57a15f2e3a48cf041a0b72
Felipe Sales <sales.felipeb@gmail.com>
2019-02-26 18:47:58 -0300

    WIp

--- node/dataSources/profile.ts
+++ node/dataSources/profile.ts
@@ -1,66 +1,62 @@
-import { RequestOptions, RESTDataSource } from 'apollo-datasource-rest'
-import { forEachObjIndexed, join } from 'ramda'
+import { HttpClient, IOContext, IODataSource } from '@vtex/api'
+import * as queryStringBuilder from 'qs'
 
-interface Address {
-  id: string
-  userId: string
-  receiverName?: string
-  complement?: string
-  neighborhood?: string
-  country?: string
-  state?: string
-  number?: string
-  street?: string
-  postalCode?: string
-  city?: string
-  reference?: string
-  addressName?: string
-  addressType?: string
-  geoCoordinate?: string
-}
+export class ProfileDataSource extends IODataSource {
+  protected httpClientFactory = forLegacy
 
-export class ProfileDataSource extends RESTDataSource<Context> {
-  public getProfileInfo = (userEmail: string, customFields?) => {
-    return this.get(join(',', [`CL/search?email=${userEmail}&_fields=userId,id,firstName,lastName,birthDate,gender,homePhone,businessPhone,document,email,isCorporate,tradeName,corporateName,stateRegistration,corporateDocument,profilePicture`, customFields]))
-    .then((data) => data[0])
-  }
+  public getProfileInfo = (userEmail: string, customFields?: string) => {
+    const queryString = queryStringBuilder.stringify({
+      extraFields: customFields,
+    })
 
-  public updateProfileInfo = (profile) => {
-    return this.patch(`CL/documents/${profile.id}`, profile)
-  }
-
-  public getAddress = (addressId: string) => {
-    return this.get(`AD/documents/${addressId}`)
-  }
-
-  public updateAddress = (address: Address) => {
-    return this.patch(`AD/documents/${address.id}`, address)
-  }
-
-  public deleteAddress = (addressId: string) => {
-    return this.delete(`AD/documents/${addressId}`)
-  }
-
-  public getUserAddresses = (userId: string) => {
-    return this.get(`AD/search?userId=${userId}&_fields=userId,id,receiverName,complement,neighborhood,country,state,number,street,postalCode,city,reference,addressName,addressType,geoCoordinate`)
-  }
-
-  get baseURL() {
-    const { vtex: { account } } = this.context
-
-    return `http://api.vtex.com/${account}/dataentities`
-  }
-
-  protected willSendRequest(request: RequestOptions) {
-    const { vtex: { authToken } } = this.context
-
-    forEachObjIndexed(
-      (value: string, header) => request.headers.set(header, value),
-      {
-        'Proxy-Authorization': authToken,
-        'VtexIdclientAutCookie': authToken,
-        'X-Vtex-Proxy-To': `https://api.vtex.com`,
+    return this.http.get(
+      `${userEmail}/personalData${queryString ? `?${queryString}` : ''}`, {
+        headers: withHeadersFromContext(this.context)
       }
     )
   }
+
+  public getUserAddresses = (userEmail: string) => {
+    return this.http.get(`${userEmail}/addresses`, {
+      headers: withHeadersFromContext(this.context)
+    })
+  }
+
+  public getUserPayments = (userEmail: string) => {
+    return this.http.get(`${userEmail}/vcs-checkout`, {
+      headers: withHeadersFromContext(this.context)
+    })
+  }
+
+  public updateProfileInfo = (userEmail: string, profile: Profile, customFields?: string) => {
+    const queryString = queryStringBuilder.stringify({
+      extraFields: customFields,
+    })
+
+    return this.http.post(`${userEmail}/personalData${queryString ? `?${queryString}` : ''}`, profile, {
+      headers: withHeadersFromContext(this.context)
+    })
+  }
+
+  public updateAddress = (userEmail: string, addressesData) => {
+    return this.http.post(`${userEmail}/addresses`, addressesData, {
+      headers: withHeadersFromContext(this.context)
+    })
+  }
+}
+
+
+function withHeadersFromContext({ account, authToken }: IOContext) {
+  return ({
+    'Proxy-Authorization': authToken,
+    'VtexIdClientAutCookie': authToken,
+    'X-Vtex-Proxy-To': `http://${account}.vtexcommercestable.com.br`,
+  })
+}
+
+function forLegacy({ options, context }) {
+  const { account = '' } = context || {}
+  const baseURL = `http://${account}.vtexcommercestable.com.br/api/profile-system/pvt/profiles`
+
+  return HttpClient.forLegacy(baseURL, options || {} as any)
 }
