9a1f58fd8c53886e1ec37d4c06a06c95764ab682
Felipe Sales <sales.felipeb@gmail.com>
2019-03-19 13:11:09 -0300

    Revert "[ON HOLD] Removing subscriptions related queries"

---
+++ node/dataSources/subscriptionsGroup.ts
@@ -0,0 +1,40 @@
+import { RequestOptions } from 'apollo-datasource-rest'
+import { forEachObjIndexed } from 'ramda'
+
+import { RESTDataSource } from './RESTDataSource'
+
+interface RetryArgs {
+  orderGroup: string
+  instanceId: string
+  workflowId: string
+}
+
+export class SubscriptionsGroupDataSource extends RESTDataSource {
+  public retry = ({ orderGroup, instanceId, workflowId }: RetryArgs) => {
+    return this.post(
+      `${orderGroup}/instances/${instanceId}/workflow/${workflowId}/retry`,
+      undefined,
+      {metric: 'SubscriptionsGroup-retry'}
+    )
+  }
+
+  get baseURL() {
+    const { vtex: { account } } = this.context
+
+    return `http://${account}.vtexcommercestable.com.br/api/rns/subscriptions-group`
+  }
+
+  protected willSendRequest(request: RequestOptions) {
+    const { cookies, vtex: { account, authToken } } = this.context
+    const client = cookies.get('VtexIdclientAutCookie')
+
+    forEachObjIndexed(
+      (value: string, header) => request.headers.set(header, value),
+      {
+        'Cookie': `VtexIdClientAutCookie=${client}`,
+        'Proxy-Authorization': authToken,
+        'X-Vtex-Proxy-To': `https://${account}.vtexcommercestable.com.br`,
+      }
+    )
+  }
+}
