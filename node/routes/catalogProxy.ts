a0459f1b6eb7b795e0daa5853db2774584410e32
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-03-22 17:08:53 -0300

    Pass all the specificationGroups by graphql

--- node/routes/catalogProxy.ts
+++ node/routes/catalogProxy.ts
@@ -8,14 +8,22 @@ const MAX_AGE_S = 2 * 60
 const STALE_IF_ERROR_S = 20 * 60
 
 export const catalogProxy = async (ctx: Context) => {
+<<<<<<< HEAD
   const {vtex: {account, authToken, production, route: {params: {path}}, segmentToken}, query, method} = ctx
 
   const isGoCommerce = Functions.isGoCommerceAcc(ctx)
 
   const [host, basePath] = isGoCommerce
-    ? ['api.gocommerce.com', `${account}/search`]
-    : [`${account}.vtexcommercestable.com.br`, 'api/catalog_system']
+=======
+  const { vtex: { account, authToken, production, route: { params: { path } } }, headers: { cookie }, query } = ctx
 
+  //TODO: REMOVER ANTES DE JOGAR NA MASTER
+  const [host, basePath] = Functions.isGoCommerceAcc(ctx)
+>>>>>>> Pass all the specificationGroups by graphql
+    ? ['api.gocommerce.com', `${account}/search`]
+    : [`${account}.vtexcommercebeta.com.br`, 'api/catalog_system']
+
+<<<<<<< HEAD
   const cookie = segmentToken && {Cookie: `vtex_segment=${segmentToken}`}
   const params = {
     ...query,
@@ -23,16 +31,26 @@ export const catalogProxy = async (ctx: Context) => {
   }
 
   const {data, headers, status} = await axios.request({
+=======
+  const { data, headers } = await axios.request({
+>>>>>>> Pass all the specificationGroups by graphql
     baseURL: `http://${host}/${basePath}`,
     headers: {
       'Authorization': authToken,
       'Proxy-Authorization': authToken,
       'X-VTEX-Proxy-To': `https://${host}`,
+<<<<<<< HEAD
       ...cookie,
     },
     method: isGoCommerce ? 'GET' : method,
     params,
     paramsSerializer: (p) => qs.stringify(p, {arrayFormat: 'repeat'}),
+=======
+      ...cookie && { cookie },
+    },
+    params: query,
+    paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat' }),
+>>>>>>> Pass all the specificationGroups by graphql
     timeout: TIMEOUT_MS,
     url: encodeURI((path as any).trim()),
   })
