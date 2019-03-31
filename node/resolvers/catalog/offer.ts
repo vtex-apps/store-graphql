8fe25b8aecd12bd7841a55bdd6ba5b545f1692a9
Bruno Dias <bruno.dias@vtex.com.br>
2019-03-31 12:32:48 -0300

    Add discountHighlights field

--- node/resolvers/catalog/offer.ts
+++ node/resolvers/catalog/offer.ts
@@ -1,4 +1,5 @@
-import {comparator, filter, gte, head, lte, sort} from 'ramda'
+import { comparator, filter, gte, head, lte, sort } from 'ramda'
+// import { renameKeysWith } from '../../utils';
 
 const InstallmentsCriteria = {
   ALL: 'ALL',
@@ -8,17 +9,21 @@ const InstallmentsCriteria = {
 
 export const resolvers = {
   Offer: {
-    Installments: ({Installments}: any, {criteria, rates}: any) => {
+    Installments: ({ Installments }: any, { criteria, rates }: any) => {
       if (criteria === InstallmentsCriteria.ALL) {
         return Installments
       }
       const filteredInstallments = !rates
         ? Installments
-        : filter(({InterestRate}) => !InterestRate, Installments)
+        : filter(({ InterestRate }) => !InterestRate, Installments)
 
       const compareFunc = criteria === InstallmentsCriteria.MAX ? gte : lte
       const byNumberOfInstallments = comparator((previous: any, next) => compareFunc(previous.NumberOfInstallments, next.NumberOfInstallments))
       return [head(sort(byNumberOfInstallments, filteredInstallments))]
     },
+    DiscountHighLight: (data: any) => {
+      console.log("Olá >>>>>>", data)
+      // return map(renameKeysWith('Name'), DiscountHighLight)
+    }
   }
 }
