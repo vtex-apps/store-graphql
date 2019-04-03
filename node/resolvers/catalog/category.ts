140086b2b4056d88125ca8d3778dc0a009fffc8f
Rerisson Matos <rerisson.matos@splab.ufcg.edu.br>
2019-04-03 14:36:23 -0300

    Add resolver for categories's children

--- node/resolvers/catalog/category.ts
+++ node/resolvers/catalog/category.ts
@@ -1,5 +1,4 @@
-import { compose, last, prop, split } from 'ramda'
-
+import { compose, last, path, prop, split, find } from 'ramda'
 import { toIOMessage } from '../../utils/ioMessage'
 
 const lastSegment = compose<string, string[], string>(last, split('/'))
@@ -8,14 +7,56 @@ export const resolvers = {
   Category: {
     cacheId: prop('id'),
 
-    href: prop('url'),
+    href: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
+      const categories = await catalog.categories(3) as Category[]
+
+      const flattenCategories = categories.reduce(
+        (acc : Category[], category) => acc.concat(category, category.children),
+        []
+      )
+
+      const category = find(
+        (c : Category) => c.id === id,
+        flattenCategories
+      ) || { url: '' }
+
+      return category.url.replace(
+        /https:\/\/[A-z0-9]+\.vtexcommercestable\.com\.br/,
+        ''
+      )
+    },
 
     metaTagDescription: prop('MetaTagDescription'),
 
     name: ({name}: any, _: any, ctx: Context) => toIOMessage(ctx, name, `category-${name}`),
 
-    slug: ({url}: any) => url ? lastSegment(url) : null,
+    slug: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
+      const categories = await catalog.categories(3) as Category[]
+
+      const flattenCategories = categories.reduce(
+        (acc : Category[], category) => acc.concat(category, category.children),
+        []
+      )
+
+      const category = find(
+        (c : Category) => c.id === id,
+        flattenCategories
+      ) || { url: '' }
+
+      return category.url ? lastSegment(category.url) : null
+    },
 
     titleTag: prop('Title'),
-  }
+
+    children: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
+      const categories = await catalog.categories(3) as Category[]
+
+      const category = find(
+        (c : Category) => c.id === id,
+        categories
+      ) || {}
+      
+      return path(['children'], category)
+    },
+  },
 }
