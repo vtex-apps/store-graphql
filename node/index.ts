9d8631c47ac24bcdcab13bea6a5547d7e90da2ee
Guilherme Rodrigues <gadr90@gmail.com>
2019-04-09 13:24:09 -0300

    Add segment cache

--- node/index.ts
+++ node/index.ts
@@ -1,17 +1,23 @@
 import './globals'
 
-import { IOClients, Service } from '@vtex/api'
+import { IOClients, LRUCache, Service } from '@vtex/api'
 
 import { dataSources } from './dataSources'
 import { schemaDirectives } from './directives'
 import { resolvers } from './resolvers'
 import { catalogProxy } from './routes/catalogProxy'
 
+const TWO_SECONDS_MS =  2 * 1000
 const FOUR_SECONDS_MS =  4 * 1000
 
 const retryConfig = {
   retries: 1,
 }
+
+// Segments are small and immutable.
+const MAX_SEGMENT_CACHE = 10000
+const segmentCache = new LRUCache<string, any>({max: MAX_SEGMENT_CACHE})
+metrics.trackCache('segment', segmentCache)
 
 export { Runtime } from '@vtex/api'
 
@@ -22,6 +28,10 @@ export default new Service<IOClients, void, CustomContext>({
         retryConfig,
         timeout: FOUR_SECONDS_MS,
       },
+      segment: {
+        memoryCache: segmentCache,
+        timeout: TWO_SECONDS_MS,
+      }
     }
   },
   graphql: {
