b7466e80f83b678f467ffdcf35432c6c300ddf5c
Guilherme Rodrigues <gadr90@gmail.com>
2019-04-09 11:26:59 -0300

    Fix typings

--- node/dataSources/index.ts
+++ node/dataSources/index.ts
@@ -5,7 +5,6 @@ import { DocumentDataSource } from './document'
 import { IdentityDataSource } from './identity'
 import { LicenseManagerDataSource } from './licenseManager'
 import { LogisticsDataSource } from './logistics'
-import { Messages } from './messages'
 import { OMSDataSource } from './oms'
 import { PortalDataSource } from './portal'
 import { ProfileDataSource } from './profile'
@@ -19,7 +18,6 @@ export const dataSources = () => ({
   identity: new IdentityDataSource(),
   licenseManager: new LicenseManagerDataSource(),
   logistics: new LogisticsDataSource(),
-  messages: new Messages(undefined, { metrics }),
   oms: new OMSDataSource(),
   portal: new PortalDataSource(),
   profile: new ProfileDataSource(undefined, { metrics }),
