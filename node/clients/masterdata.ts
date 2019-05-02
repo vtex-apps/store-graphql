295130d2aa3a4ca1bfbb0b414af430d159645bcd
Felipe Sales <sales.felipeb@gmail.com>
2019-05-02 10:20:29 -0300

    Fix prettier

--- node/clients/masterdata.ts
+++ node/clients/masterdata.ts
@@ -1,10 +1,18 @@
-import { AuthenticationError, ForbiddenError, InstanceOptions, IOContext, JanusClient, RequestConfig, UserInputError } from '@vtex/api'
+import {
+  AuthenticationError,
+  ForbiddenError,
+  InstanceOptions,
+  IOContext,
+  JanusClient,
+  RequestConfig,
+  UserInputError,
+} from '@vtex/api'
 import { AxiosError } from 'axios'
 import FormData from 'form-data'
 import { mergeAll, zipObj } from 'ramda'
 
 interface PaginationArgs {
-  page: number,
+  page: number
   pageSize: number
 }
 
@@ -24,9 +32,8 @@ interface KeyValue {
  * Convert a list of fields like [ {key: 'propertyName', value: 'String'}, ... ]
  * to a JSON format.
  */
-const parseFieldsToJson = (fields: KeyValue[]) => mergeAll(
-  fields.map((field: any) => zipObj([field.key], [field.value])),
-)
+const parseFieldsToJson = (fields: KeyValue[]) =>
+  mergeAll(fields.map((field: any) => zipObj([field.key], [field.value])))
 
 const statusToError = (e: any) => {
   if (!e.response) {
@@ -53,7 +60,7 @@ const paginationArgsToHeaders = ({ page, pageSize }: PaginationArgs) => {
   const startIndex = (page - 1) * pageSize
   const endIndex = startIndex + pageSize
   return {
-    'REST-Range': `resources=${startIndex}-${endIndex}`
+    'REST-Range': `resources=${startIndex}-${endIndex}`,
   }
 }
 
@@ -62,44 +69,63 @@ export class MasterData extends JanusClient {
     super(ctx, {
       ...options,
       headers: {
-        ...options && options.headers,
+        ...(options && options.headers),
         ...{ Accept: 'application/vnd.vtex.ds.v10+json' },
-        ...ctx.adminUserAuthToken ? { VtexIdclientAutCookie: ctx.adminUserAuthToken } : null,
-        ...ctx.storeUserAuthToken ? { [`VtexIdclientAutCookie_${ctx.account}`]: ctx.storeUserAuthToken } : null,
+        ...(ctx.adminUserAuthToken
+          ? { VtexIdclientAutCookie: ctx.adminUserAuthToken }
+          : null),
+        ...(ctx.storeUserAuthToken
+          ? { [`VtexIdclientAutCookie_${ctx.account}`]: ctx.storeUserAuthToken }
+          : null),
       },
     })
   }
 
-  public getDocument = (acronym: string, id: string, fields: string[]) => this.get<Document>(
-    this.routes.document(acronym, id),
-    { metric: 'masterdata-getDocument', params: { _fields: fields } }
-  )
+  public getDocument = (acronym: string, id: string, fields: string[]) =>
+    this.get<Document>(this.routes.document(acronym, id), {
+      metric: 'masterdata-getDocument',
+      params: { _fields: fields },
+    })
 
-  public createDocument = (acronym: string, fields: KeyValue[]) => this.post<Document>(
-    this.routes.documents(acronym),
-    { metric: 'masterdata-createDocument', params: parseFieldsToJson(fields) }
-  )
+  public createDocument = (acronym: string, fields: KeyValue[]) =>
+    this.post<Document>(this.routes.documents(acronym), {
+      metric: 'masterdata-createDocument',
+      params: parseFieldsToJson(fields),
+    })
 
-  public updateDocument = (acronym: string, id: string, fields: KeyValue[]) => this.patch<Document>(
-    this.routes.document(acronym, id),
-    { metric: 'masterdata-updateDocument', params: parseFieldsToJson(fields) }
-  )
+  public updateDocument = (acronym: string, id: string, fields: KeyValue[]) =>
+    this.patch<Document>(this.routes.document(acronym, id), {
+      metric: 'masterdata-updateDocument',
+      params: parseFieldsToJson(fields),
+    })
 
-  public searchDocuments = (acronym: string, fields: string[], where: string, pagination: PaginationArgs) => this.get<any[]>(
-    this.routes.search(acronym),
-    { headers: paginationArgsToHeaders(pagination), metric: 'masterdata-searchDocuments', params: { _fields: fields, _where: where } }
-  )
+  public searchDocuments = (
+    acronym: string,
+    fields: string[],
+    where: string,
+    pagination: PaginationArgs
+  ) =>
+    this.get<any[]>(this.routes.search(acronym), {
+      headers: paginationArgsToHeaders(pagination),
+      metric: 'masterdata-searchDocuments',
+      params: { _fields: fields, _where: where },
+    })
 
-  public deleteDocument = (acronym: string, id: string) => this.delete(
-    this.routes.document(acronym, id),
-    { metric: 'masterdata-deleteDocument' }
-  )
+  public deleteDocument = (acronym: string, id: string) =>
+    this.delete(this.routes.document(acronym, id), {
+      metric: 'masterdata-deleteDocument',
+    })
 
-  public uploadAttachment = (acronym: string, id: string, fields: string, formData: FormData) => this.post<any>(
-    this.routes.attachments(acronym, id, fields),
-    formData,
-    { headers: formData.getHeaders(), metric: 'masterdata-uploadAttachment' }
-  )
+  public uploadAttachment = (
+    acronym: string,
+    id: string,
+    fields: string,
+    formData: FormData
+  ) =>
+    this.post<any>(this.routes.attachments(acronym, id, fields), formData, {
+      headers: formData.getHeaders(),
+      metric: 'masterdata-uploadAttachment',
+    })
 
   protected get = <T>(url: string, config?: RequestConfig) => {
     return this.http.get<T>(url, config).catch(statusToError)
@@ -120,8 +146,10 @@ export class MasterData extends JanusClient {
   private get routes() {
     const base = '/api/dataentities'
     return {
-      attachments: (acronym: string, id: string, fields: string) => `${acronym}/documents/${id}/${fields}/attachments`,
-      document: (acronym: string, id: string) => `${base}/${acronym}/documents/${id}`,
+      attachments: (acronym: string, id: string, fields: string) =>
+        `${acronym}/documents/${id}/${fields}/attachments`,
+      document: (acronym: string, id: string) =>
+        `${base}/${acronym}/documents/${id}`,
       documents: (acronym: string) => `${base}/${acronym}/documents`,
       search: (acronym: string) => `${base}/${acronym}/search`,
     }
