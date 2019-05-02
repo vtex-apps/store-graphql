0c6e5a7d2a8fc377a53f33aafc2611bd2449731b
Felipe Sales <sales.felipeb@gmail.com>
2019-05-02 19:00:59 -0300

    Fix document, and documents resolver

--- node/resolvers/document/index.ts
+++ node/resolvers/document/index.ts
@@ -1,43 +1,74 @@
 import { prop, union } from 'ramda'
-import {parseFieldsToJson }  from '../../utils'
+import { parseFieldsToJson } from '../../utils'
 import { mapKeyValues } from '../../utils/object'
 
 export const queries = {
-  documents: async (_: any, args: any, { clients: { masterdata } }: Context) => {
-    const { acronym, fields, page, pageSize, where } = args
+  documents: (
+    _: any,
+    { acronym, fields, page, pageSize, where }: DocumentsArgs,
+    { clients: { masterdata } }: Context
+  ) => {
     const fieldsWithId = union(fields, ['id'])
-    const data = await masterdata.searchDocuments(acronym, fieldsWithId as any, where, { page, pageSize })
-    return data.map((doc: any) => ({
-      cacheId: doc.id,
-      fields: mapKeyValues(doc),
-      id: doc.id,
-    }))
+
+    return masterdata
+      .searchDocuments(acronym, fieldsWithId, where, { page, pageSize })
+      .then(data =>
+        data.map((doc: any) => ({
+          cacheId: doc.id,
+          fields: mapKeyValues(doc),
+          id: doc.id,
+        }))
+      )
   },
 
-  document: async (_: any, args: any, { clients: { masterdata } }: Context) => {
-    const { acronym, fields, id } = args
-    const data = await masterdata.getDocument(acronym, id, fields)
-    return { id, cacheId: id, fields: mapKeyValues(data) }
-  },
+  document: (
+    _: any,
+    { acronym, fields, id }: DocumentArgs,
+    { clients: { masterdata } }: Context
+  ) =>
+    masterdata
+      .getDocument(acronym, id, fields)
+      .then(data => ({ id, cacheId: id, fields: mapKeyValues(data) })),
 }
 
 export const mutations = {
-  createDocument: async (_: any, args: any, { clients: { masterdata } }: Context) => {
-    const { acronym, document: { fields } } = args
-    const { Id, Href, DocumentId } = await masterdata.createDocument(acronym, fields)
+  createDocument: async (
+    _: any,
+    { acronym, document: { fields } }: any,
+    { clients: { masterdata } }: Context
+  ) => {
+    const { Id, Href, DocumentId } = await masterdata.createDocument(
+      acronym,
+      fields
+    )
     return { cacheId: DocumentId, id: Id, href: Href, documentId: DocumentId }
   },
 
-  updateDocument: async (_: any, args: any, { clients: { masterdata } }: Context) => {
-    const { acronym, document: { fields } } = args
+  updateDocument: async (
+    _: any,
+    args: any,
+    { clients: { masterdata } }: Context
+  ) => {
+    const {
+      acronym,
+      document: { fields },
+    } = args
     const id = prop('id', parseFieldsToJson(fields)) as string
-    const { Id, Href, DocumentId } = await masterdata.updateDocument(acronym, id, fields)
+    const { Id, Href, DocumentId } = await masterdata.updateDocument(
+      acronym,
+      id,
+      fields
+    )
     return { cacheId: DocumentId, id: Id, href: Href, documentId: DocumentId }
   },
 
-  deleteDocument: async (_: any, args: any, { clients: { masterdata } }: Context) => {
+  deleteDocument: async (
+    _: any,
+    args: any,
+    { clients: { masterdata } }: Context
+  ) => {
     const { acronym, documentId } = args
     await masterdata.deleteDocument(acronym, documentId)
     return { id: documentId }
-  }
+  },
 }
