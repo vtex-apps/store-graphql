c4c1c958c60ba9b24419ab44c08de13146f61793
Breno Calazans <brenocalazans@gmail.com>
2019-05-03 15:09:13 -0300

    Fix department URL

--- node/resolvers/catalog/category.ts
+++ node/resolvers/catalog/category.ts
@@ -1,4 +1,4 @@
-import { compose, find, last, path, prop, split } from 'ramda'
+import { compose, last, path, prop, split } from 'ramda'
 import { toIOMessage } from '../../utils/ioMessage'
 
 const lastSegment = compose<string, string[], string>(last, split('/'))
@@ -6,48 +6,74 @@ const lastSegment = compose<string, string[], string>(last, split('/'))
 interface Category {
   id: string,
   url: string,
+  name: string,
   children: Category[],
+}
+
+type CategoryMap = Record<string, Category>
+
+const getCategoryInfo = async (catalog: any, id: string) => {
+  const categories = await catalog.categories(3) as Category[]
+
+  const mapCategories = categories.reduce((mapCategories: CategoryMap, department) => {
+    mapCategories[department.id] = {
+      ...department,
+    }
+
+    mapCategories = department.children.reduce((mapCategories: CategoryMap, category) => {
+      mapCategories[category.id] = {
+        ...category,
+      }
+
+      mapCategories = category.children.reduce((mapCategories, subCategory) => {
+        mapCategories[subCategory.id] = {
+          ...subCategory,
+        }
+
+        return mapCategories
+      }, mapCategories)
+
+      return mapCategories
+    }, mapCategories)
+
+    return mapCategories
+  }, {}) as CategoryMap
+
+  const category = mapCategories[id] || { url: '' }
+
+  return category
+}
+
+function cleanUrl(url: string) {
+  return url.replace(
+    /https:\/\/[A-z0-9]+\.vtexcommercestable\.com\.br/,
+    ''
+  )
 }
 
 export const resolvers = {
   Category: {
     cacheId: prop('id'),
 
-    href: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
-      const categories = await catalog.categories(3) as Category[]
+    href: async ({ id }: Category, _: any, { dataSources: { catalog } }: any) => {
+      const category = await getCategoryInfo(catalog, id)
 
-      const flattenCategories = categories.reduce(
-        (acc : Category[], cat) => acc.concat(cat, cat.children),
-        []
-      )
+      const path = cleanUrl(category.url)
 
-      const category = find(
-        (c : Category) => c.id === id,
-        flattenCategories
-      ) || { url: '' }
+      const isDepartment = path.slice(1).indexOf('/') === -1
+      if (isDepartment) {
+        return path + '/d'
+      }
 
-      return category.url.replace(
-        /https:\/\/[A-z0-9]+\.vtexcommercestable\.com\.br/,
-        ''
-      )
+      return path
     },
 
     metaTagDescription: prop('MetaTagDescription'),
 
-    name: ({name}: any, _: any, ctx: Context) => toIOMessage(ctx, name, `category-${name}`),
+    name: ({ name }: Category, _: any, ctx: Context) => toIOMessage(ctx, name, `category-${name}`),
 
-    slug: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
-      const categories = await catalog.categories(3) as Category[]
-
-      const flattenCategories = categories.reduce(
-        (acc : Category[], c) => acc.concat(c, c.children),
-        []
-      )
-
-      const category = find(
-        (c : Category) => c.id === id,
-        flattenCategories
-      ) || { url: '' }
+    slug: async ({ id }: Category, _: any, { dataSources: { catalog } }: any) => {
+      const category = await getCategoryInfo(catalog, id)
 
       return category.url ? lastSegment(category.url) : null
     },
@@ -55,18 +81,8 @@ export const resolvers = {
     titleTag: prop('Title'),
 
     children: async ({ id }: any, _: any, { dataSources: { catalog } }: any) => {
-      const categories = await catalog.categories(3) as Category[]
-      
-      const flattenCategories = categories.reduce(
-        (acc : Category[], c) => acc.concat(c, c.children),
-        []
-      )
+      const category = await getCategoryInfo(catalog, id)
 
-      const category = find(
-        (c : Category) => c.id === id,
-        flattenCategories
-      ) || {}
-    
       return path(['children'], category)
     },
   },
