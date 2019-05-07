b8f6105a695b9830d4a2947c22a7e041908e6f01
Tiago Gimenes <tlgimenes@gmail.com>
2019-05-07 10:37:49 -0300

    making requested changes

--- node/utils/ioMessage.ts
+++ node/utils/ioMessage.ts
@@ -1,6 +1,7 @@
 import { Segment } from '@vtex/api'
 import { prop } from 'ramda'
-import { extractSlug } from '../resolvers/catalog';
+
+import { extractSlug } from '../resolvers/catalog'
 
 const localeFromDefaultSalesChannel = (segment: Segment) =>
   segment.getSegmentByToken(null).then(prop('cultureInfo'))
@@ -11,13 +12,13 @@ export const toIOMessage = async (segment: Segment, content: string, id: string)
   id,
 })
 
-export const toProductIOMessage = (field: string) => async (segment: Segment, content: string, link: string) => toIOMessage(
+export const toProductIOMessage = (field: string) => (segment: Segment, content: string, link: string) => toIOMessage(
   segment,
   content,
   `slug.${extractSlug({href: link})}::Product-${field}`
 )
 
-export const toCategoryIOMessage = (field: string) => async (segment: Segment, content: string, id: string) => toIOMessage(
+export const toCategoryIOMessage = (field: string) => (segment: Segment, content: string, id: string) => toIOMessage(
   segment,
   content,
   `id.${id}::Category-${field}`
