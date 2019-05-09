40fba8be67f6ce83d6e6a12d0a2afff6a29313fa
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-05-09 20:26:58 -0300

    Fix mutations, change schema a little bit

--- node/resolvers/document/services.ts
+++ node/resolvers/document/services.ts
@@ -1,9 +1,9 @@
 import { ResolverError } from '@vtex/api'
-import { union } from 'ramda'
+import { compose, map, union, prop, replace } from 'ramda'
 import FormData from 'form-data'
 
 import { generateRandomName } from '../../utils'
-import { mapKeyValues } from '../../utils/object'
+import { mapKeyValues, parseFieldsToJson } from '../../utils/object'
 
 export async function uploadAttachment(args: any, ctx: Context) {
   const {
@@ -46,18 +46,21 @@ export async function uploadAttachment(args: any, ctx: Context) {
 }
 
 export function retrieveDocument({
+  args: { acronym, fields, id },
   context: {
     clients: { masterdata },
   },
-  args: { acronym, fields, id },
 }: {
-  context: Context
   args: DocumentArgs
+  context: Context
 }) {
-  return masterdata.getDocument(acronym, id, fields).then(data => ({
-    id: id,
+  return masterdata.getDocument(acronym, id, fields).then(data =>{
+    console.log(data)
+    return {
+    id,
     fields: mapKeyValues(data),
-  }))
+  }
+  })
 }
 
 export function retrieveDocuments({
@@ -66,8 +69,8 @@ export function retrieveDocuments({
     clients: { masterdata },
   },
 }: {
-  context: Context
   args: DocumentsArgs
+  context: Context
 }) {
   const fieldsWithId = union(fields, ['id'])
 
@@ -75,8 +78,55 @@ export function retrieveDocuments({
     page,
     pageSize,
   })
+    .then(map((document: any) => ({id: document.id, fields: mapKeyValues(document)})))
+}
+
+export function createDocument({
+  args: { acronym, document: { fields } },
+  context : {
+    clients: { masterdata }
+  },
+}: {
+  args: CreateDocumentArgs
+  context: Context
+}) {
+  return masterdata.createDocument(acronym, parseFieldsToJson(fields))
+    .then(data => ({ id: getCleanId(acronym, data), href: prop('Href', data)}))
+}
+
+
+export function updateDocument({
+  args: { acronym, document: { fields }, id },
+  context : {
+    clients: { masterdata }
+  },
+}: {
+  args: UpdateDocumentArgs
+  context: Context
+}) {
+  return masterdata.updateDocument(acronym, id, parseFieldsToJson(fields))
+    .then(() => ({ id }))
+}
+
+export function deleteDocument({
+  args: { acronym, id },
+  context : {
+    clients: { masterdata }
+  },
+}: {
+  args: DeleteDocumentArgs
+  context: Context
+}) {
+  return masterdata.deleteDocument(acronym, id)
 }
 
 function getFileExtension(fileName: any) {
   return fileName.match(/\.[0-9a-z]+$/i)[0]
+}
+
+function getCleanId(acronym: string, data: {Id: string}) {
+  return compose<any, any, any>(
+    replace(new RegExp(`${acronym}-`), ''),
+    prop('Id')
+  )(data)
 }
