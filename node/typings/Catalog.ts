170dd26ed229218070eab33ce3e1e0fefd9a4d47
Joao Fidelis <joaogfidelis@gmail.com>
2019-05-10 14:05:08 -0300

    move catalog types to typings folder, add more types in resolvers

---
+++ node/typings/Catalog.ts
@@ -0,0 +1,39 @@
+interface ProductsArgs {
+  query: string
+  category: string
+  specificationFilters: string[]
+  priceRange: string
+  collection: string
+  salesChannel: string
+  orderBy: string
+  from: number
+  to: number
+  map: string
+  hideUnavailableItems: boolean
+}
+
+interface Metadata {
+  metaTagDescription?: string
+  titleTag?: string
+}
+
+interface Brand {
+  id: string
+  name: string
+  isActive: boolean
+  title?: string
+  metaTagDescription?: string
+}
+
+interface Category {
+  id: string
+  name: string
+  url: string
+  children: Category[]
+}
+
+interface FacetsArgs {
+  facets: string //deprecated!
+  query: string
+  map: string
+}
