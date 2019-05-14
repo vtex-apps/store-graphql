e3137ee15cce4fcd52f0921d931df865e1eb605d
Felipe Sales <sales.felipeb@gmail.com>
2019-05-14 10:40:10 -0300

    Using new client architecture

--- node/resolvers/fileManager/exceptions/fileNotFound.ts
+++
@@ -1,9 +0,0 @@
-export class FileNotFound extends Error {
-  constructor(
-    public extensions: any,
-    public message = 'File Not Found',
-    public statusCode = 404
-  ) {
-    super(message)
-  }
-}
