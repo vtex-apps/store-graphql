b77f7967fc7445772fdc97d05951417604aace64
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-05-14 16:43:44 -0300

    Add document/attachment.ts again

---
+++ node/resolvers/document/attachment.ts
@@ -0,0 +1,48 @@
+import { ResolverError } from '@vtex/api'
+import FormData from 'form-data'
+
+import { generateRandomName } from '../../utils'
+
+export async function uploadAttachment(args: any, ctx: Context) {
+  const {
+    clients: { masterdata },
+  } = ctx
+  const { acronym, documentId, field, file } = args
+  const { createReadStream, filename, mimetype } = await file
+  const buffer = (await new Promise((resolve, reject) => {
+    const bufs: any[] = []
+    const stream = createReadStream()
+    stream.on('data', (d: any) => bufs.push(d))
+    stream.on('end', () => {
+      resolve(Buffer.concat(bufs))
+    })
+    stream.on('error', reject)
+  })) as Buffer
+
+  const formData = new FormData()
+
+  const randomName = generateRandomName() + getFileExtension(filename)
+
+  formData.append(field, buffer, {
+    contentType: mimetype,
+    filename: randomName,
+    knownLength: buffer.byteLength,
+  })
+
+  const response = await masterdata.uploadAttachment(
+    acronym,
+    documentId,
+    field,
+    formData
+  )
+
+  if (response) {
+    throw new ResolverError(response)
+  }
+
+  return { filename: randomName, mimetype }
+}
+
+function getFileExtension(fileName: any) {
+  return fileName.match(/\.[0-9a-z]+$/i)[0]
+}
