d504323afc0aa2743fe35c416895ed7a15969f1d
Felipe Sales <sales.felipeb@gmail.com>
2019-05-21 10:39:30 -0300

    Removing console log

--- node/clients/utils.ts
+++ node/clients/utils.ts
@@ -2,12 +2,13 @@ import { AxiosError } from 'axios'
 import { AuthenticationError, ForbiddenError, UserInputError } from '@vtex/api'
 
 export function statusToError(e: any) {
-  console.log('error', e)
   if (!e.response) {
     throw e
   }
+
   const { response } = e as AxiosError
   const { status } = response!
+
   if (status === 401) {
     throw new AuthenticationError(e)
   }
@@ -17,5 +18,6 @@ export function statusToError(e: any) {
   if (status === 400) {
     throw new UserInputError(e)
   }
+
   throw e
 }
