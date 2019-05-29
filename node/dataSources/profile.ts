8701fa770962e5f8773e5dea5558f26f723ef329
Felipe Sales <sales.felipeb@gmail.com>
2019-05-29 00:20:04 -0300

    Prettier

--- node/dataSources/profile.ts
+++ node/dataSources/profile.ts
@@ -1,12 +1,31 @@
-import { HttpClient, HttpClientFactory, InstanceOptions, IOContext, IODataSource } from '@vtex/api'
+import {
+  HttpClient,
+  HttpClientFactory,
+  InstanceOptions,
+  IOContext,
+  IODataSource,
+} from '@vtex/api'
 import * as queryStringBuilder from 'qs'
 
-const forProfile: HttpClientFactory = ({context, options}) => context &&
-  HttpClient.forExternal(`http://${context.account}.vtexcommercestable.com.br/api/profile-system/pvt/profiles`, context, {...options, headers: {
-    'Proxy-Authorization': context.authToken,
-    'VtexIdClientAutCookie': context.authToken,
-    'X-Vtex-Proxy-To': `http://${context.account}.vtexcommercestable.com.br`,
-  }, metrics})
+const forProfile: HttpClientFactory = ({ context, options }) =>
+  context &&
+  HttpClient.forExternal(
+    `http://${
+      context.account
+    }.vtexcommercestable.com.br/api/profile-system/pvt/profiles`,
+    context,
+    {
+      ...options,
+      headers: {
+        'Proxy-Authorization': context.authToken,
+        VtexIdClientAutCookie: context.authToken,
+        'X-Vtex-Proxy-To': `http://${
+          context.account
+        }.vtexcommercestable.com.br`,
+      },
+      metrics,
+    }
+  )
 
 export class ProfileDataSource extends IODataSource {
   protected httpClientFactory = forProfile
@@ -23,20 +42,20 @@ export class ProfileDataSource extends IODataSource {
     return this.http.get(
       `${userEmail}/personalData${queryString ? `?${queryString}` : ''}`,
       {
-        metric: 'profile-system-getProfileInfo'
+        metric: 'profile-system-getProfileInfo',
       }
     )
   }
 
   public getUserAddresses = (userEmail: string) => {
     return this.http.get(`${userEmail}/addresses`, {
-      metric: 'profile-system-getUserAddresses'
+      metric: 'profile-system-getUserAddresses',
     })
   }
 
   public getUserPayments = (userEmail: string) => {
     return this.http.get(`${userEmail}/vcs-checkout`, {
-      metric: 'profile-system-getUserPayments'
+      metric: 'profile-system-getUserPayments',
     })
   }
 
@@ -53,29 +72,33 @@ export class ProfileDataSource extends IODataSource {
       `${userEmail}/personalData${queryString ? `?${queryString}` : ''}`,
       profile,
       {
-        metric: 'profile-system-updateProfileInfo'
+        metric: 'profile-system-updateProfileInfo',
       }
     )
   }
 
   public updateAddress = (userEmail: string, addressesData: any) => {
     return this.http.post(`${userEmail}/addresses`, addressesData, {
-      metric: 'profile-system-updateAddress'
+      metric: 'profile-system-updateAddress',
     })
   }
 
   public deleteAddress = (userEmail: string, addressName: string) => {
     return this.http.delete(`${userEmail}/addresses/${addressName}`, {
-      metric: 'profile-system-deleteAddress'
+      metric: 'profile-system-deleteAddress',
     })
   }
 
   public updatePersonalPreferences = (
     userEmail: string,
-    personalPreferences: PersonalPreferences,
+    personalPreferences: PersonalPreferences
   ) => {
-    return this.http.post(`${userEmail}/personalPreferences/`, personalPreferences, {
-      metric: 'profile-system-subscribeNewsletter'
-    })
+    return this.http.post(
+      `${userEmail}/personalPreferences/`,
+      personalPreferences,
+      {
+        metric: 'profile-system-subscribeNewsletter',
+      }
+    )
   }
 }
