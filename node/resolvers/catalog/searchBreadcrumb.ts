d8cb094d20b3325c26f93b4791fc34a8c94b88cc
Joao Fidelis <joaogfidelis@gmail.com>
2019-06-25 10:15:43 -0300

    decore unit name on search breadcrumb, small code improvements

--- node/resolvers/catalog/searchBreadcrumb.ts
+++ node/resolvers/catalog/searchBreadcrumb.ts
@@ -1,6 +1,6 @@
-import { toLower } from 'ramda'
-import { findCategoryInTree, getBrandFromSlug } from "./utils"
-import { toCategoryIOMessage, toClusterIOMessage } from "../../utils/ioMessage"
+import { toLower, equals } from 'ramda'
+import { findCategoryInTree, getBrandFromSlug } from './utils'
+import { toCategoryIOMessage, toClusterIOMessage } from '../../utils/ioMessage'
 
 interface BreadcrumbParams {
   queryUnit: string
@@ -14,29 +14,55 @@ interface BreadcrumbParams {
 }
 
 const findClusterNameFromId = (products: Product[], clusterId: string) => {
-  const productWithCluster = products.find(({ productClusters }) => !!productClusters[clusterId])
+  const productWithCluster = products.find(
+    ({ productClusters }) => !!productClusters[clusterId]
+  )
   return productWithCluster && productWithCluster.productClusters[clusterId]
 }
 
-const sliceAndJoin = (array: string[], max: number, joinChar: string) => array.slice(0, max).join(joinChar)
+const sliceAndJoin = (array: string[], max: number, joinChar: string) =>
+  array.slice(0, max).join(joinChar)
+
+const isCategoryMap = equals('c')
+const isBrandMap = equals('b')
+const isProductClusterMap = equals('productClusterIds')
 
 export const resolvers = {
   SearchBreadcrumb: {
     name: async (obj: BreadcrumbParams, _: any, ctx: Context) => {
-      const {clients: {segment}} = ctx
-      const { queryUnit, mapUnit, index, queryArray, categories, categoriesSearched, products } = obj
+      const {
+        clients: { segment },
+      } = ctx
+      const {
+        queryUnit,
+        mapUnit,
+        index,
+        queryArray,
+        categories,
+        categoriesSearched,
+        products,
+      } = obj
       const defaultName = queryArray[index]
-      if (mapUnit === 'productClusterIds') {
+      if (isProductClusterMap(mapUnit)) {
         const clusterName = findClusterNameFromId(products, queryUnit)
         if (clusterName) {
           return toClusterIOMessage(segment, clusterName, queryUnit)
         }
       }
-      if (mapUnit === 'c') {
-        const queryPosition = categoriesSearched.findIndex(cat => cat === queryUnit)
-        const category = findCategoryInTree(categories, categoriesSearched.slice(0, queryPosition + 1))
+      if (isCategoryMap(mapUnit)) {
+        const queryPosition = categoriesSearched.findIndex(
+          cat => cat === queryUnit
+        )
+        const category = findCategoryInTree(
+          categories,
+          categoriesSearched.slice(0, queryPosition + 1)
+        )
         if (category) {
-          return toCategoryIOMessage('name')(segment, category.name, category.id)
+          return toCategoryIOMessage('name')(
+            segment,
+            category.name,
+            category.id
+          )
         }
         // if cant find a category, we should try to see if its a product cluster
         const clusterName = findClusterNameFromId(products, queryUnit)
@@ -44,21 +70,33 @@ export const resolvers = {
           return toClusterIOMessage(segment, clusterName, queryUnit)
         }
       }
-      if (mapUnit === 'b') {
+      if (isBrandMap(mapUnit)) {
         const brand = await getBrandFromSlug(toLower(queryUnit), ctx)
         return brand ? brand.name : defaultName
       }
-      return defaultName
+      return defaultName && decodeURI(defaultName)
     },
-    href: ({ index, queryArray, mapArray, mapUnit, queryUnit }: BreadcrumbParams) => {
-      if (index === 0 && mapUnit === 'c') {
+    href: ({
+      index,
+      queryArray,
+      mapArray,
+      mapUnit,
+      queryUnit,
+    }: BreadcrumbParams) => {
+      if (index === 0 && isCategoryMap(mapUnit)) {
         return `/${queryUnit}/d`
       }
-      if (index === 0 && mapUnit === 'b') {
+      if (index === 0 && isBrandMap(mapUnit)) {
         return `/${queryUnit}/b`
       }
-      return `/${sliceAndJoin(queryArray, index+1, '/')}?map=${sliceAndJoin(mapArray, index+1, ',')}`
-    }
-      
-  }
+      if (mapArray.every(isCategoryMap)) {
+        return `/${sliceAndJoin(queryArray, index + 1, '/')}`
+      }
+      return `/${sliceAndJoin(queryArray, index + 1, '/')}?map=${sliceAndJoin(
+        mapArray,
+        index + 1,
+        ','
+      )}`
+    },
+  },
 }
