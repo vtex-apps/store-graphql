2306b4b8ce64595a6660db761cf222aa492f10f4
Joao Fidelis <joaogfidelis@gmail.com>
2019-06-28 17:41:39 -0300

    get old code back for GC

--- node/resolvers/catalog/productSearch.ts
+++ node/resolvers/catalog/productSearch.ts
@@ -1,5 +1,6 @@
 import { path, zip } from 'ramda'
 import { IOResponse } from '@vtex/api'
+import { Functions } from '@gocommerce/utils'
 
 interface ProductSearchParent {
   productsRaw: IOResponse<Product[]>
@@ -24,7 +25,8 @@ export const resolvers = {
     products: path(['productsRaw', 'data']),
     breadcrumb: async (
       { translatedArgs, productsRaw: { data: products } }: ProductSearchParent,
-      _: any
+      _: any,
+      { vtex: { account }, clients: { catalog } }: Context
     ) => {
       const queryAndMap = zip(
         translatedArgs.query
@@ -36,6 +38,14 @@ export const resolvers = {
       const categoriesSearched = queryAndMap
         .filter(([_, m]) => m === 'c')
         .map(([q]) => q)
+      const categoriesCount = translatedArgs.map
+        .split(',')
+        .filter(m => m === 'c').length
+      const categories =
+        !!categoriesCount && Functions.isGoCommerceAcc(account)
+          ? await catalog.categories(categoriesCount)
+          : []
+
       return queryAndMap.map(
         ([queryUnit, mapUnit]: [string, string], index: number) => ({
           queryUnit,
@@ -43,6 +53,7 @@ export const resolvers = {
           index,
           queryArray: translatedArgs.query.split('/'),
           mapArray: translatedArgs.map.split(','),
+          categories,
           categoriesSearched,
           products,
         })
