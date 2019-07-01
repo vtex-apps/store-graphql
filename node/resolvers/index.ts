772d8ae0aa3e8d41d98a9c3ab5ce5f8d44dc4634
arthurepc <arthurcosta@vtex.com.br>
2019-07-01 18:59:04 -0300

    Add siteConfigs query

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -1,24 +1,43 @@
 import { mutations as authMutations, queries as authQueries } from './auth'
-import { fieldResolvers as benefitsFieldResolvers, queries as benefitsQueries } from './benefits'
-import { fieldResolvers as catalogFieldResolvers, queries as catalogQueries } from './catalog'
+import {
+  fieldResolvers as benefitsFieldResolvers,
+  queries as benefitsQueries,
+} from './benefits'
+import {
+  fieldResolvers as catalogFieldResolvers,
+  queries as catalogQueries,
+} from './catalog'
 import {
   fieldResolvers as checkoutFieldResolvers,
   mutations as checkoutMutations,
   queries as checkoutQueries,
 } from './checkout'
 import {
+<<<<<<< HEAD
   fieldResolvers as documentFieldResolvers,
   mutations as documentMutations,
   queries as documentQueries } from './document'
+=======
+  mutations as documentMutations,
+  queries as documentQueries,
+} from './document'
+>>>>>>> Add siteConfigs query
 import { mutation as listMutations, queries as listQueries } from './list'
-import { fieldResolvers as logisticsResolvers, queries as logisticsQueries } from './logistics'
+import {
+  fieldResolvers as logisticsResolvers,
+  queries as logisticsQueries,
+} from './logistics'
 import { queries as omsQueries } from './oms'
 import {
   fieldResolvers as profileFieldResolvers,
   mutations as profileMutations,
   queries as profileQueries,
 } from './profile'
-import { mutations as sessionMutations, queries as sessionQueries } from './session'
+import { resolvers as portalResolvers } from './portal'
+import {
+  mutations as sessionMutations,
+  queries as sessionQueries,
+} from './session'
 
 // eslint-disable-next-line no-global-assign
 Promise = require('bluebird')
@@ -49,5 +68,6 @@ export const resolvers = {
     ...sessionQueries,
     ...listQueries,
     ...omsQueries,
-  }
+    ...portalResolvers,
+  },
 }
