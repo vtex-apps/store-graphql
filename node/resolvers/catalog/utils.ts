d127f5d46901cf845699c1a6867d8b9db11e093e
Camilo Camargo <camilo.camargo@vtex.com>
2019-07-04 11:49:01 -0500

    Sending discountHighlights and Teasers data in Products info

--- node/resolvers/catalog/utils.ts
+++ node/resolvers/catalog/utils.ts
@@ -10,6 +10,15 @@ export enum CatalogCrossSellingTypes {
   suggestions = 'suggestions',
 }
 
+export enum DiscountsLabels {
+  name = '<Name>k__BackingField',
+  conditions = '<Conditions>k__BackingField',
+  minimumQuantity = '<MinimumQuantity>k__BackingField',
+  parameters = '<Parameters>k__BackingField',
+  value = '<Value>k__BackingField',
+  effects = '<Effects>k__BackingField'
+}
+
 const pageTypeMapping: Record<string, string> = {
   Brand: 'brand',
   Department: 'department',
