f25b045e500f19a67f8d36932ec5b6d04ece04e2
Tiago Gimenes <tlgimenes@gmail.com>
2019-07-08 20:55:18 -0300

    fixes mismatching types with node-vtex-api

--- node/utils/cookie.ts
+++ node/utils/cookie.ts
@@ -1,4 +1,3 @@
-import * as Cookies from "cookies"
 import { parse } from 'cookie'
 import { keys } from 'ramda'
 
@@ -29,7 +28,7 @@ const CHECKOUT_COOKIE = 'checkout.vtex.com'
 
 const checkoutCookieFormat = (orderFormId: string) => `${CHECKOUT_COOKIE}=__ofid=${orderFormId};`
 
-const getOrderFormIdFromCookie = (cookies: Cookies) => {
+const getOrderFormIdFromCookie = (cookies: Context['cookies']) => {
   const cookie = cookies.get(CHECKOUT_COOKIE)
   return cookie && cookie.split('=')[1]
 }
