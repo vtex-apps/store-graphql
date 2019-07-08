8a421c7fb7e77ce9a1c276685d640d3686e93de6
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-07-08 17:01:34 -0300

    Remove console.log

--- node/resolvers/catalog/productSearch.ts
+++ node/resolvers/catalog/productSearch.ts
@@ -35,7 +35,6 @@ export const resolvers = {
           .map(decodeURIComponent),
         translatedArgs.map.split(',')
       )
-      console.log(queryAndMap)
       const categoriesSearched = queryAndMap
         .filter(([_, m]) => m === 'c')
         .map(([q]) => q)
