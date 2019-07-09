c6aad3c253e9296ef6f1172e4a37cc34cb4466ff
Felipe Sales <sales.felipeb@gmail.com>
2019-07-09 18:55:39 -0300

    Port logistics to IO client.

---
+++ node/clients/logistics.ts
@@ -0,0 +1,58 @@
+import {
+  InstanceOptions,
+  IOContext,
+  JanusClient,
+  RequestConfig,
+} from '@vtex/api'
+
+import { statusToError } from '../utils'
+import { LogisticOuput } from '../resolvers/logistics/types'
+
+const FOUR_SECONDS = 4 * 1000
+
+export class LogisticsClient extends JanusClient {
+  public constructor(ctx: IOContext, options?: InstanceOptions) {
+    super(ctx, {
+      ...options,
+      headers: {
+        ...(options && options.headers),
+        VtexIdclientAutCookie: ctx.authToken,
+      },
+      timeout: FOUR_SECONDS,
+    })
+  }
+
+  public pickupById = (id: string) =>
+    this.get(this.routes.pickUpById(id), {
+      metric: 'logistics-pickupById',
+    })
+
+  public nearPickupPoints = (
+    lat: string,
+    long: string,
+    maxDistance = 50
+  ): Promise<LogisticOuput | void> =>
+    this.get(this.routes.nearPickupPoints(lat, long, maxDistance), {
+      metric: 'logistics-nearPickupPoints',
+    })
+
+  public shipping = () =>
+    this.get(this.routes.shipping, {
+      metric: 'logistics-shipping',
+    })
+
+  protected get = <T>(url: string, config?: RequestConfig) =>
+    this.http.get<T>(url, config).catch(statusToError)
+
+  private get routes() {
+    const basePVT = '/api/logistics'
+
+    return {
+      shipping: `${basePVT}/pub/shipping/configuration`,
+      nearPickupPoints: (lat: string, long: string, maxDistance: number) =>
+        `${basePVT}/pvt/configuration/pickuppoints/_search?&page=1&pageSize=100&lat=${lat}&$lon=${long}&maxDistance=${maxDistance}`,
+      pickUpById: (id: string) =>
+        `${basePVT}/pvt/configuration/pickuppoints/${id}`,
+    }
+  }
+}
--- node/dataSources/index.ts
+++ node/dataSources/index.ts
@@ -1,7 +1,5 @@
 import { IdentityDataSource } from './identity'
-import { LogisticsDataSource } from './logistics'
 
 export const dataSources = () => ({
   identity: new IdentityDataSource(),
-  logistics: new LogisticsDataSource(),
 })
