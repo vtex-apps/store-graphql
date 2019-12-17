b1916e474ebebd6d2c1e196b76b789b2ad17e859
Joao Fidelis <joaogfidelis@gmail.com>
2019-12-17 18:02:27 -0300

    get new segment if undefined

--- node/directives/withSegment.ts
+++ node/directives/withSegment.ts
@@ -7,11 +7,11 @@ import atob from 'atob'
  * If the getSegment API evolves to do more than a decode we must stop decoding it here and start calling the API once again.
  */
 export class WithSegment extends SchemaDirectiveVisitor {
-  public visitFieldDefinition (field: GraphQLField<any, any>) {
-    const {resolve = defaultFieldResolver} = field
+  public visitFieldDefinition(field: GraphQLField<any, any>) {
+    const { resolve = defaultFieldResolver } = field
     field.resolve = async (root, args, ctx: Context, info) => {
-      const {vtex: {segmentToken}} = ctx
-      ctx.vtex.segment = segmentToken ? JSON.parse(atob(segmentToken)) : undefined
+      const { vtex: { segmentToken }, clients: { segment } } = ctx
+      ctx.vtex.segment = segmentToken ? JSON.parse(atob(segmentToken)) : (await segment.getSegment())
       return resolve(root, args, ctx, info)
     }
   }
