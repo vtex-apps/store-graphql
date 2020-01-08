7bbceae58a4c0aa965e42beb597e60a285a9c91a
Joao Fidelis <joaogfidelis@gmail.com>
2020-01-08 19:15:46 -0300

    break in two ifs

--- node/resolvers/checkout/index.ts
+++ node/resolvers/checkout/index.ts
@@ -305,7 +305,10 @@ export const mutations: Record<string, Resolver> = {
       }
 
       const atLeastOneValidField = Object.values(newMarketingData).some(value => {
-        if ((value == null || value === '') && (Array.isArray(value) && value.length === 0)) {
+        if (value == null || value === '') {
+          return false
+        }
+        if (Array.isArray(value) && value.length === 0) {
           return false
         }
         return true
