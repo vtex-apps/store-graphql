2dc39fb3b8f15aeb8306ccd9c9ce752dd842a7c2
Joao Fidelis <joaogfidelis@gmail.com>
2020-02-12 17:26:00 -0300

    create and use toVtexAssets

--- node/directives/index.ts
+++ node/directives/index.ts
@@ -1,8 +1,10 @@
 import { WithCurrentProfile } from './withCurrentProfile'
 import { WithSegment } from './withSegment'
 import { WithOrderFormId } from './withOrderFormId'
+import { ToVtexAssets } from './toVtexAssets'
 
 export const schemaDirectives = {
+  toVtexAssets: ToVtexAssets,
   withCurrentProfile: WithCurrentProfile,
   withSegment: WithSegment,
   withOrderFormId: WithOrderFormId,
