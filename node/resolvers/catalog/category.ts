b88d5f2ac1ab7ae0494a958632946da88e05bce5
Victor Raposo <vwraposo@gmail.com>
2020-09-23 14:47:23 -0300

    Transaltes category slug

--- node/resolvers/catalog/category.ts
+++ node/resolvers/catalog/category.ts
@@ -1,12 +1,25 @@
-import { compose, last, prop, split } from 'ramda'
+import { prop } from 'ramda'
 
 import { getCategoryInfo } from './utils'
 import { formatTranslatableProp } from '../../utils/i18n'
+import { catalogSlugify } from './slug'
 
-const lastSegment = compose<string, string[], string>(
-  last,
-  split('/')
-)
+const getTypeForCategory = (url: string) => {
+  const correctUrl = url.startsWith('/') ? url : `/${url}`
+  switch (correctUrl.split('/').length) {
+    case 2:
+      return 'department'
+    case 3:
+      return 'category'
+    default:
+      return 'subcategory'
+  }
+}
+
+const lastSegment = (route: string) => {
+  const splittedSegments = route.includes('/') ? route.split('/') : route
+  return splittedSegments[splittedSegments.length - 1]
+}
 
 function cleanUrl(url: string) {
   return url.replace(/^.*(vtexcommercestable\.com\.br)/, '')
@@ -65,15 +78,19 @@ export const resolvers = {
     ),
 
     slug: async (
-      { id, url }: SafeCategory,
+      { name, id, url }: SafeCategory,
       _: any,
-      { clients: { catalog } }: Context
+      { vtex: { tenant, binding }, clients: { catalog, rewriter } }: Context
     ) => {
       if (url == null) {
         const category = await getCategoryInfo(catalog, id, 4)
         url = category.url
       }
-      return url ? lastSegment(url) : null
+      if (!tenant || !binding || tenant?.locale === binding?.locale || !binding.id ) {
+        return catalogSlugify(name).toLowerCase()
+      }
+      const translatedRoute = await rewriter.getRoute(id.toString(), getTypeForCategory(url), binding.id) || url
+      return lastSegment(translatedRoute)
     },
 
     children: async (
