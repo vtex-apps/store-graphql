6d62408b8198d3ec5c3dca73c753781b539103a1
Ícaro Azevedo <icazevedo10@gmail.com>
2021-11-22 17:11:28 -0300

    Adding auth logs to graphql

---
+++ node/directives/authorizationMetrics.ts
@@ -0,0 +1,52 @@
+import { defaultFieldResolver, GraphQLField, GraphQLResolveInfo } from 'graphql'
+import { SchemaDirectiveVisitor } from 'graphql-tools'
+
+function checkForAuthorization(ctx: any, info: GraphQLResolveInfo) {
+  const {
+    request: {
+      headers: {
+        'user-agent': userAgent,
+        'x-forwarded-for': forwardedFor,
+        'x-forwarded-host': forwardedHost,
+        'x-forwarded-path': forwardedPath,
+        'x-vtex-caller': vtexCaller,
+        origin,
+        Authorization,
+      },
+    },
+    vtex: { logger, adminUserAuthToken, storeUserAuthToken },
+  } = ctx
+
+  const vtexIdToken =
+    ctx.cookies.get('VtexIdclientAutCookie') ?? ctx.get('VtexIdclientAutCookie')
+
+  if (!vtexIdToken) {
+    logger.warn({
+      message: 'Private route being accessed by unauthorized user',
+      userAgent,
+      forwardedFor,
+      forwardedHost,
+      forwardedPath,
+      origin,
+      vtexCaller,
+      Authorization,
+      adminUserAuthToken,
+      storeUserAuthToken,
+      queryName: info?.fieldName,
+      queryParentType: info?.parentType.name,
+      queryReturnType: info?.returnType.toString(),
+    })
+  }
+}
+
+export class AuthorizationMetrics extends SchemaDirectiveVisitor {
+  public visitFieldDefinition(field: GraphQLField<any, any>) {
+    const { resolve = defaultFieldResolver } = field
+
+    field.resolve = async (root, args, ctx, info) => {
+      checkForAuthorization(ctx, info)
+
+      return resolve(root, args, ctx, info)
+    }
+  }
+}
--- node/directives/index.ts
+++ node/directives/index.ts
@@ -2,10 +2,12 @@ import { WithCurrentProfile } from './withCurrentProfile'
 import { WithSegment } from './withSegment'
 import { WithOrderFormId } from './withOrderFormId'
 import { ToVtexAssets } from './toVtexAssets'
+import { AuthorizationMetrics } from './authorizationMetrics'
 
 export const schemaDirectives = {
   toVtexAssets: ToVtexAssets,
   withCurrentProfile: WithCurrentProfile,
   withSegment: WithSegment,
   withOrderFormId: WithOrderFormId,
+  withAuthMetrics: AuthorizationMetrics,
 }
