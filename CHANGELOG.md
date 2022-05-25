b780fa63a74e21ede9d3478f95c32d8edd26fb7c
Victor Miranda <victor2142@gmail.com>
2022-05-25 10:48:18 -0300

    Revert changes from PR #604

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-## [2.152.1] - 2022-05-23
+## [2.152.1] - 2022-05-23 [YANKED]
 
 ### Fixed
 - Discrepancy in simulation's Price due to unitMultiplier
--- node/utils/simulation.ts
+++ node/utils/simulation.ts
@@ -68,10 +68,18 @@ export const orderFormItemToSeller = (
     logisticsInfo: any[]
   }
 ) => {
+  const unitMultiplier = orderFormItem.unitMultiplier ?? 1
   const [logisticsInfo] = orderFormItem.logisticsInfo
 
   const commertialOffer = {
-    Price: orderFormItem.price / 100,
+    Price: orderFormItem.priceDefinition?.calculatedSellingPrice
+      ? Number(
+          (
+            orderFormItem.priceDefinition.calculatedSellingPrice /
+            (unitMultiplier * 100)
+          ).toFixed(2)
+        )
+      : orderFormItem.price / 100,
     PriceValidUntil: orderFormItem.priceValidUntil,
     ListPrice: orderFormItem.listPrice / 100,
     PriceWithoutDiscount: orderFormItem.price / 100,
