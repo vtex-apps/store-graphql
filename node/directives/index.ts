ae2e36c8ae4a968b83d296f71228a4dbbeee07b0
Daniel Zanzini <daniel.zanzini@vtex.com.br>
2023-01-17 10:27:24 -0300

    Adds directive to monitor origin of orders endpoint

--- node/directives/index.ts
+++ node/directives/index.ts
@@ -4,6 +4,7 @@ import { WithOrderFormId } from './withOrderFormId'
 import { ToVtexAssets } from './toVtexAssets'
 import { AuthorizationMetrics } from './authorizationMetrics'
 import { WithOwnerId } from './withOwnerId'
+import { MonitorOrigin } from './monitorOrigin'
 
 export const schemaDirectives = {
   toVtexAssets: ToVtexAssets,
@@ -12,4 +13,5 @@ export const schemaDirectives = {
   withOrderFormId: WithOrderFormId,
   withOwnerId: WithOwnerId,
   withAuthMetrics: AuthorizationMetrics,
+  monitorOrigin: MonitorOrigin,
 }
